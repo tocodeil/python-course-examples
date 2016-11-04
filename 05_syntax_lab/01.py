c99e39a2e55c24107eb6b94f980565c7ce45d8d7
ynonp <ynonperek@gmail.com>
2016-11-04 10:48:06 +0200

    fixed comment

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,7 +1,11 @@
 """
 Write a program that reads 10 numbers from
 the user and prints the largest one
+
+For example if the largest number a user has typed
+is 74, program should print:
+
+    Max number = 74
 """
 
-print "Max number = %d" % max_number
 
