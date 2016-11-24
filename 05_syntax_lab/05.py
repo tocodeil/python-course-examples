89a764fb4a8148d049d38799d84473f484f4a265
ItayElbaz <itayelbaz7@gmail.com>
2016-11-24 10:55:06 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,9 @@
-"""
-Write a program that generates random integers in a loop
-until it finds a number that is divisible by: 7, 13, and 15.
-"""
+import random
+
+number = random.randint(1,1000001)
+
+while number % 7 != 0 or number % 13 != 0 or number % 15 != 0:
+    number = random.randint(1, 1000001)
+else:
+    print number
 
