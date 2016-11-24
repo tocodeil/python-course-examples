c87e0f4285c2c522293775e1ea43f48c218ced69
ItayElbaz <itayelbaz7@gmail.com>
2016-11-24 16:04:00 +0200

    Solution for: 06_modules_lab/02_bonus.py

--- 06_modules_lab/02_bonus.py
+++ 06_modules_lab/02_bonus.py
@@ -13,3 +13,18 @@ https://docs.python.org/2.7/tutorial/errors.html
 And then continue to writing the code
 """
 
+import sys
+
+#if sys.argv[1] !=  or sys.argv[2]
+if sys.argv[1].isdigit() == False or sys.argv[2].isdigit() == False:
+    print " "
+    print " "
+    print "Sorry, One of the object you entered is not a number"
+    print "Please try as follow : 'python 02_bonus.py <number1> <number2>' "
+    print " "
+    print " "
+
+x = sys.argv[1]
+y = sys.argv[2]
+
+print int(x) * int(y)
