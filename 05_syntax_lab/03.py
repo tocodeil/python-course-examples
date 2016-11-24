08d7c4e0b4ebcba1c64b521dfd8d7ba518cd206d
ItayElbaz <itayelbaz7@gmail.com>
2016-11-24 10:54:57 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -1,8 +1,12 @@
-"""
-Write a program that generates a random number
-between 1 and 10,000,
-and prints the sum of its digits.
-For example if the number was: 2345
-the result should be: 14.
-"""
+import random
 
+rand_num = random.randint(1,10001)
+
+rand_num = str(rand_num)
+sum_num = 0
+
+for i in range(len(rand_num)):
+    sum_num = sum_num + int(rand_num[i])
+
+print "The random number is : %s" % (rand_num)
+print "The sum of his digits : %s " % (sum_num)
