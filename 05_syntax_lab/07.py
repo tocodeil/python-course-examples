6199e0fc5ed2d07d4baa01230ab39e69a587546d
ItayElbaz <itayelbaz7@gmail.com>
2016-11-24 10:55:11 +0200

    Solution for: 05_syntax_lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,7 +1,15 @@
-""" Write a program that selects a random number
-and asks the user to guess it.
-After each guess print a hint "too large" or "too small" to the user.
-Bonus: To make things interesting, the program should cheat once in a white
-"""
+import random
 
+number = random.randint(1,101)
+
+print "We choose a number between 1 to 100"
+guess = int(raw_input("Guess the number :"))
+
+while guess != number:
+    if guess > number:
+        guess = int(raw_input("Too high.. Try again :"))
+    elif guess < number:
+        guess = int(raw_input("Too low.. Try again :"))
+
+print "Correct ! This is the number !"
 
