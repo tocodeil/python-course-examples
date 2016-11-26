ffc7c2a576c216af97dee349062cb17f7422d5d0
Gabor Szabo <gabor@szabgab.com>
2016-11-26 22:47:19 +0200

    add some tests

--- 06_modules_lab/spec.py
+++ 06_modules_lab/spec.py
@@ -25,6 +25,25 @@ class TestEx2(unittest.TestCase):
         line = proc.stdout.readline()
         self.assertIn('-17', line)
 
+class TestEx2Bonus(unittest.TestCase):
+    def test_print_sum_positive(self):
+        proc = subprocess.Popen([sys.executable, '02_bonus.py', '5', '12'],
+                                stdout=subprocess.PIPE)
+        line = proc.stdout.readline()
+        self.assertIn('17', line)
+
+    def test_print_sum_string_first(self):
+        proc = subprocess.Popen([sys.executable, '02_bonus.py', 'abc', '12'],
+                                stdout=subprocess.PIPE)
+        line = proc.stdout.readline()
+        self.assertIn('Syntax error: please enter integers only.\n', line)
+
+    def test_print_sum_float_first(self):
+        proc = subprocess.Popen([sys.executable, '02_bonus.py', '2.3', '12'],
+                                stdout=subprocess.PIPE)
+        line = proc.stdout.readline()
+        self.assertIn('Syntax error: please enter integers only.\n', line)
+
 
 if __name__ == '__main__':
     unittest.main()
