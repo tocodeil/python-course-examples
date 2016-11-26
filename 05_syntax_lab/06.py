5be6cb7a01fe793c0106861a5555d03832c9a470
mesika <yaniv.mesika@gmail.com>
2016-11-27 00:32:33 +0800

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -7,3 +7,18 @@ For example if the numbers were 4 and 6,
 program should print 12.
 """
 
+import random
+
+
+num1 = random.randint(1, 10)
+num2 = random.randint(1, 10)
+
+print num1
+print num2
+
+for i in range(1, num1 * num2 + 1):
+    if i % num1 == 0 and i % num2 == 0:
+        print i
+        break
+
+
