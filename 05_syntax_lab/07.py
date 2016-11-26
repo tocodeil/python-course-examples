1089b4d6751214da01d85a1b35b5b74ad52ddfe7
mesika <yaniv.mesika@gmail.com>
2016-11-27 00:32:36 +0800

    Solution for: 05_syntax_lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -5,3 +5,31 @@ Bonus: To make things interesting, the program should cheat once in a white
 """
 
 
+import random
+
+randomNumber = random.randint(1,100)
+
+print "Guess the number (between 1 and 100):"
+
+userNumber = -1
+
+while True:
+    userNumber = int(raw_input())
+
+    cheatingFactor = random.randint(0, 100)
+    if cheatingFactor % 11 == 0:
+        print "*too large"
+        continue
+    elif cheatingFactor %12 == 0:
+        print "*too small"
+        continue
+
+    if userNumber > randomNumber:
+        print "too large"
+    elif userNumber < randomNumber:
+        print "too small"
+    else:
+        print "Bingo!"
+        break
+
+
