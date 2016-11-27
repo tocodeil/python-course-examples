d0fb89c567dee4d6190fcb7c8c9b8e8e6206728a
gbarzilay <galia.barzilay@verint.com>
2016-11-27 14:15:50 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -5,4 +5,12 @@ and prints the sum of its digits.
 For example if the number was: 2345
 the result should be: 14.
 """
+from random import randint
 
+number = randint(1,10000)
+
+sum = 0
+for digit in str(number):
+	sum += int(digit)
+
+print sum
