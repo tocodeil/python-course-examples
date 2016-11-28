0104dce6f4835a3dff44c669cc7c967d03c119d5
Chagit171 <Chagit171@gmail.com>
2016-11-29 01:16:20 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -6,14 +6,12 @@ until it finds a number that is divisible by: 7, 13, and 15.
 
 
 from random import randint
-
+random_number = 0
 while True:
     random_number= randint(1,1000000)
-    print random_number
     if random_number%7 != 0: continue
     elif random_number%13 !=0 : continue
-    elif random_number%15 != 0 : 
-        continue
+    elif random_number%15 != 0 : continue
     else:
          break
-print ("I found a magic number!!!")
+print random_number
