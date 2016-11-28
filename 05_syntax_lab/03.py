789fbe51bfc795e2b5670d02f2179c50ea0d2812
ItayElbaz <itayelbaz7@gmail.com>
2016-11-28 16:41:02 +0200

    Update 03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -5,8 +5,8 @@ rand_num = random.randint(1,10000)
 rand_num = str(rand_num)
 sum_num = 0
 
-for i in range(len(rand_num)):
-    sum_num = sum_num + int(rand_num[i])
+for c in rand_num:
+    sum_num = sum_num + int(c)
 
 print "The random number is : %s" % (rand_num)
 print "The sum of his digits : %s " % (sum_num)
