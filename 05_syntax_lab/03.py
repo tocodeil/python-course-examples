c12ae0901a852409d6716f2200367233d48d3eea
Chagit171 <Chagit171@gmail.com>
2016-11-28 23:28:18 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -6,3 +6,22 @@ For example if the number was: 2345
 the result should be: 14.
 """
 
+
+
+
+
+
+
+
+from random import randint
+sum=0
+for x in range(0,7):
+    random_number=randint(1,100)
+    print(random_number)
+    sum+=random_number
+print "The sum is:" , sum
+if sum%7==0:
+    print "Boom"
+
+
+    
