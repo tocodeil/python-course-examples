5f2a6c9e0373ac1d0db184aab9c2c23d0313a822
Chagit171 <Chagit171@gmail.com>
2016-11-28 23:28:29 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -7,3 +7,18 @@ For example if the numbers were 4 and 6,
 program should print 12.
 """
 
+
+
+from random import randint 
+
+num1= randint(1,10)
+num2= randint(1,10)
+
+max = num1 if num1>num2 else num2
+
+multi=max
+print num1, "and ", num2
+
+while (multi%num1 != 0 or multi%num2 != 0):
+        multi+=max
+print (multi)
