7f920f4a3b68d67eb5d7205f08a73a3f7827a219
Chagit171 <Chagit171@gmail.com>
2016-11-28 23:28:06 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -4,3 +4,21 @@ between 1 and 100, and prints their sum.
 If the sum is divisible by 7, also print the word "Boom".
 """
 
+
+
+
+
+
+
+from random import randint
+sum=0
+for x in range(0,7):
+    random_number=randint(1,100)
+    print(random_number)
+    sum+=random_number
+print "The sum is:" , sum
+if sum%7==0:
+    print "Boom"
+
+
+    
