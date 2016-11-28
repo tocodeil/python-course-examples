0ae701f0fb3a8ef062ea5cfdfaaad87318706475
Chagit171 <Chagit171@gmail.com>
2016-11-29 01:16:16 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -9,19 +9,12 @@ the result should be: 14.
 
 
 
-
-
-
-
 from random import randint
+
 sum=0
-for x in range(0,7):
-    random_number=randint(1,100)
-    print(random_number)
-    sum+=random_number
-print "The sum is:" , sum
-if sum%7==0:
-    print "Boom"
-
-
-    
+number=randint(1,10000)
+print number
+while number:
+    sum+= (number%10)
+    number/=10
+print sum
