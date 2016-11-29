e1aa36a0e3d53a7c81eb6d7bc9274964bfbf290b
User1978 <User1978@users.noreply.github.com>
2016-11-29 09:52:04 +0200

    Solution for: 05_syntax_lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -6,3 +6,12 @@ print all previously entered lines in reverse
 order (from last to first).
 """
 
+lines = ""
+
+while True:
+    print ("Please enter you input: ")
+    line = raw_input()
+    lines = lines + line
+    if not line: break
+
+print ("The input was: " + lines[::-1])
