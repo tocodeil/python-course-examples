b62b25856c1114cb9783eddc375b63922dbc2f9b
elhi <elhanan.rahamim@verint.com>
2016-11-29 09:54:57 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -5,4 +5,15 @@ and prints the sum of its digits.
 For example if the number was: 2345
 the result should be: 14.
 """
+import random
+number = (random.randint(1, 10000))
+sum = 0
+print number
+for i in range (5):
+    modul = number % 10
+    sum = sum + modul
+    number = number / 10
+
+print sum
+
 
