993ea8631e8fd68be4dc57d885a6f98d2a88a395
User1978 <User1978@users.noreply.github.com>
2016-11-29 09:52:06 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -3,3 +3,12 @@ Write a program that generates random integers in a loop
 until it finds a number that is divisible by: 7, 13, and 15.
 """
 
+from random import randint
+
+while True:
+    number = randint(1, 1000000)
+    if number % 7 == 0 and number % 13 == 0 and number % 15 == 0:
+        print("Bingo! The number that meets the criterion is " + str(number))
+        break
+    else:
+        continue
