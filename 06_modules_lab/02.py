a7dc7390c966d2b9cd7ff0892bc3f4729c39641d
gbarzilay <galia.barzilay@verint.com>
2016-11-29 09:40:35 +0200

    Solution for: 06_modules_lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -2,3 +2,10 @@
 and prints their sum.
 """
 
+import sys
+
+if len(sys.argv) != 3:
+    print "Usage: %s <number> <number>" % sys.argv[0]
+    sys.exit(1)
+
+print int(sys.argv[1]) + int(sys.argv[2])
