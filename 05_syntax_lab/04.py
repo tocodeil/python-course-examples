8308d688a9a51732a726d38d4bf3ad7014c60543
Lionellionelel <lionelel@gmail.com>
2016-11-29 14:18:17 +0200

    Solution for: 05_syntax_lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -1,8 +1,20 @@
-"""
-Write a program that reads lines from the user
-until an empty line is entered.
-After the user typed in an empty line,
-print all previously entered lines in reverse
-order (from last to first).
-"""
 
+myList = []
+
+while True:
+    txt = raw_input('Please enter something')
+    if not txt:
+        break
+    else:
+        myList.append(txt)
+
+
+if myList:
+    if len(myList) == 1:
+        print myList
+    else:
+        print myList
+        sorted_list = sorted(myList, reverse=True)
+        print sorted_list
+        for line in sorted_list:
+            print line
