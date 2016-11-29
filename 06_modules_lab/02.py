89e9166ce2e85de46020f63eb73a519bd90b3757
Chagit171 <Chagit171@gmail.com>
2016-11-29 09:24:57 +0200

    Solution for: 06_modules_lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -2,3 +2,13 @@
 and prints their sum.
 """
 
+import sys
+
+try:
+    num1=float(sys.argv[1])
+    num2=float(sys.argv[2])
+    print (num1+num2)
+except:
+    print"Wrong parameters, Please insert two numbers"
+
+
