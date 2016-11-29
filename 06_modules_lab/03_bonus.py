92756ceffdf3d3ebaf65226b14223ce132a9641d
gbarzilay <galia.barzilay@verint.com>
2016-11-29 09:40:38 +0200

    Solution for: 06_modules_lab/03_bonus.py

--- 06_modules_lab/03_bonus.py
+++ 06_modules_lab/03_bonus.py
@@ -11,3 +11,22 @@ its name to the user.
 Bonus: Use argparse module to parse command line arguments
 """
 
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Delete large files.')
+parser.add_argument('root', nargs='?', default='.', help='the root folder')
+parser.add_argument('size',  nargs='?', default=1000000, type=int, help='the minimal size of files to delete')
+args = parser.parse_args()
+
+for path, dirnames, filenames in os.walk(args.root):
+  for name in filenames:
+    fullPath = os.path.join(path,name)
+    size = os.path.getsize(fullPath)
+    if (size > args.size):
+      if (raw_input("Delete file %s (size=%dB)? Y/N:" % (fullPath, size)).lower() == "y"):
+        print "Deleting %s" % fullPath
+        os.remove(fullPath)
+     
+  
