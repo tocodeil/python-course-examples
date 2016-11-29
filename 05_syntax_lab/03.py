414f40e911c34b8f87cb8c9deff78e7093445643
User1978 <User1978@users.noreply.github.com>
2016-11-29 09:52:00 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -6,3 +6,18 @@ For example if the number was: 2345
 the result should be: 14.
 """
 
+from random import randint
+
+number_count = []
+random_number = str(randint(1, 10000))
+print("The random number is " + random_number)
+
+for num in random_number:
+    number_count.append(int(num))
+
+total_digits = 0
+for digit in number_count:
+    total_digits += digit
+
+print("The sum of the number's digits is " + str(total_digits))
+
