b4a39353ec5a361dff9137511da3b7724e1c85d9
shlomiiss <shlomiiss@gmail.com>
2016-11-29 16:50:59 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -1,6 +1,16 @@
+
 """
-Write a Python program that generates 7 random integers
-between 1 and 100, and prints their sum.
-If the sum is divisible by 7, also print the word "Boom".
+This code is random 7 numbers in range of 1-100 and print the sum calc of the numbers . If this sum can be divied by 7 to print Boom
 """
 
+i =  0
+random = 0
+sumnumber= 0
+from random import randint
+for i in range(1,7):
+ random = randint (1,100)
+ sumnumber += random
+ print ("The sum of the seven random numbers is", sumnumber ) 
+ if sumnumber % 7 == 0 :
+   print ("Boom the number %d is divided by 7 "  % sumnumber ) 
+ 
