ef688762c1f80516089ca352704e9b66d7426f60
User1978 <User1978@users.noreply.github.com>
2016-11-29 09:52:09 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -7,3 +7,19 @@ For example if the numbers were 4 and 6,
 program should print 12.
 """
 
+from random import randint
+
+first_number = randint(1, 10)
+print("The first random number is " + str(first_number))
+second_number = randint(1, 10)
+print("The second random number is " + str(second_number))
+common = first_number * second_number
+
+if common % 2 == 0:
+    print("The LCM is " + str(common /2))
+else:
+    for number in range (0, common):
+        if number % first_number == 0 and number % second_number == 0:
+            print("The LCM is " + str(number))
+        else:
+            continue
