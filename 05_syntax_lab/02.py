a01d421fca44682b51d3d7354282fe3e432d706d
Balinbal <inbalmiezels@gmail.com>
2016-11-30 20:44:58 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -6,8 +6,8 @@ If the sum is divisible by 7, the program print the word "Boom".
 sum=0
 import random
 
-for num in range(1,7):
-  num=random.randrange(100)
+for num in range(7):
+  num=random.randrange(1,100)
   sum=sum+num
 
 print sum
