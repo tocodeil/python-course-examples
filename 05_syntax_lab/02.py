8014eda01746c3fbc23612e40e004d5bb014821f
shlomiiss <shlomiiss@gmail.com>
2016-11-30 09:43:10 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -1,16 +1,14 @@
 
 """
-This code is random 7 numbers in range of 1-100 and print the sum calc of the numbers . If this sum can be divied by 7 to print Boom
-"""
+This code is random 7 numbers in range of 1-100 and print the sum calc of the numbers . 
+If this sum can be divied by 7 to print Boom
 
-i =  0
+"""
 random = 0
-sumnumber= 0
+sumnumber = 0
 from random import randint
-for i in range(1,7):
+for _ in range(7):
  random = randint (1,100)
  sumnumber += random
- print ("The sum of the seven random numbers is", sumnumber ) 
- if sumnumber % 7 == 0 :
-   print ("Boom the number %d is divided by 7 "  % sumnumber ) 
- 
+if (sumnumber % 7 == 0):
+   print "%s %d" % ('Boom',sumnumber )
