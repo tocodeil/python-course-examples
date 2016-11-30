bd30eadf83738310973002b1d5bacbadc6fc3d3c
ygolan99 <yariv.golan@verint.com>
2016-11-30 15:33:31 +0200

    Solution for: 05_syntax_lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,19 +1,7 @@
-"""
-Write a program that reads 10 numbers from
-the user and prints the largest one
-
-For example if the largest number a user has typed
-is 74, program should print:
-
-    Max number = 74
-"""
-
 maxnum = float('-inf')
 
 for _ in range(10):
-    num = float(raw_input())
+    num = float (raw_input())
     if num > maxnum:
         maxnum = num
-
-print "Max number = %f" % maxnum
-
+    print "max number = %f" % maxnum
