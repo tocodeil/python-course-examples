f9c78158ada7b40b6d83cdc297928b515df75ee9
ygolan99 <yariv.golan@verint.com>
2016-11-30 15:35:20 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -6,4 +6,22 @@ by both.
 For example if the numbers were 4 and 6,
 program should print 12.
 """
+x=0
+y=0
+lcm=0
+import random
+x = int(random.randint(1,10))
+y = int(random.randint(1,10))
+
+if x > y :
+    BigNum = x
+else:
+    BigNum = y
+
+while(True):
+     if((BigNum % x == 0) and (BigNum % y == 0)):
+         lcm = BigNum
+         print "the lcm for", x ,"and" ,y ,"is:" ,lcm
+         break
+     BigNum += 1
 
