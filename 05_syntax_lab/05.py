8abfc40846d935824c4fce13e744724dc3f253d8
User1978 <User1978@users.noreply.github.com>
2016-11-30 13:19:41 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -8,7 +8,5 @@ from random import randint
 while True:
     number = randint(1, 1000000)
     if number % 7 == 0 and number % 13 == 0 and number % 15 == 0:
-        print("Bingo! The number that meets the criterion is " + str(number))
+        print "Bingo! The number that meets the criterion is " + str(number)
         break
-    else:
-        continue
