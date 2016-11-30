4a950be9492a1b61b459f311e01dbe8a0ad53874
elhi <elhanan.rahamim@verint.com>
2016-11-30 07:52:07 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -6,4 +6,25 @@ by both.
 For example if the numbers were 4 and 6,
 program should print 12.
 """
-
+import random
+a = random.randint(1,10)
+b = random.randint(1,10)
+big = a * b
+print a, b
+c = a
+d = b
+while a == b:
+    print a
+    exit()
+while a > b:
+    if a <= big:
+        if c % b == 0 and c % a == 0:
+            print c
+            exit()
+    c = c +1
+while b > a:
+        if b <= big:
+            if d % b == 0 and d % a == 0:
+                print d
+                exit()
+        d = d +1
