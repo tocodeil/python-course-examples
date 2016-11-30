654d3ccc10f7845a12f1c94ef597d5f461d52d53
LiatKotigaro <liat.kotigaro@verint.com>
2016-11-30 14:56:01 +0200

    Solution for: 05_syntax_lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -5,4 +5,21 @@ After the user typed in an empty line,
 print all previously entered lines in reverse
 order (from last to first).
 """
+print "Please enter lines:"
+total_lines = ""
+while True:
+    line = raw_input()
+    if len(line) == 0:
+        break
+    if len(total_lines) == 0:
+        total_lines = line
+    else:
+        total_lines = line + '\n' + total_lines
 
+if len(total_lines) == 0:
+    print "No lines were entered"
+else:
+    lines_array = total_lines.splitlines()
+    print "The lines in reverse order:"
+    for i in range(len(lines_array)):
+        print lines_array[i]
