118625a0c209bea1ab621bf38bf5abe1b96a533a
User1978 <User1978@users.noreply.github.com>
2016-11-30 13:19:47 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -10,16 +10,13 @@ program should print 12.
 from random import randint
 
 first_number = randint(1, 10)
-print("The first random number is " + str(first_number))
+print "The first random number is " + str(first_number)
 second_number = randint(1, 10)
-print("The second random number is " + str(second_number))
-common = first_number * second_number
+print "The second random number is " + str(second_number)
 
-if common % 2 == 0:
-    print("The LCM is " + str(common /2))
-else:
-    for number in range (0, common):
-        if number % first_number == 0 and number % second_number == 0:
-            print("The LCM is " + str(number))
-        else:
-            continue
+for number in range(1, second_number + 1):
+    lcm = first_number * number
+    print lcm
+    if lcm % second_number == 0:
+        print "The LCM is " + str(lcm)
+        break
