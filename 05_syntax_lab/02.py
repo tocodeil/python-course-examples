c21bdbe23e04a83cd608af36809aa5778cf704ab
anatshapira <anatshapira8@gmail.com>
2016-11-30 23:21:02 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -1,6 +1,25 @@
 """
-Write a Python program that generates 7 random integers
-between 1 and 100, and prints their sum.
-If the sum is divisible by 7, also print the word "Boom".
+01.py
+
 """
+
+import sys
+
+if len(sys.argv) != 3:
+    print "wrong number of auguments, exiting..."
+    sys.exit()
+
+(_, first_num, sec_num) = sys.argv
+
+if not first_num.isdigit():
+    print first_num + " is not a number, exiting..."
+    sys.exit()
+
+if not sec_num.isdigit():
+    print sec_num + " is not a number, exiting..."
+    sys.exit()
+
+
+res = int(first_num) + int(sec_num)
+print " and the resualt is : " + str(res)
 
