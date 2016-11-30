4e4b2eb5aca82473eb8772540218bde9487c15f9
User1978 <User1978@users.noreply.github.com>
2016-11-30 13:19:54 +0200

    Solution for: 05_syntax_lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -7,20 +7,22 @@ Bonus: To make things interesting, the program should cheat once in a white
 from random import randint
 
 random_number = randint(1, 100)
+print random_number
 tries_allowed = 5
 
-print("I'm thinking of a number between 1 and 100. You have 5 tries to guess it\n")
+print "I'm thinking of a number between 1 and 100. You have 5 tries to guess it \n"
 
 for guess in range(tries_allowed):
     user_input = int(raw_input("Please enter your guess"))
     if user_input > random_number:
-        print("Please try harder. Your number is too high")
+        print "Please try harder. Your number is too high"
 
     elif user_input < random_number:
-        print ("Please try harder. Your number is too small")
+        print "Please try harder. Your number is too small"
 
     else:
-        print("You did it. You guessed the number was" + str(random_number))
+        print "You did it. You guessed the number was " + str(random_number)
+        break
 
 else:
-    print("You had 5 tries. I'm sorry, You are a failure")
+    print "You had 5 tries. I'm sorry, You are a failure"
