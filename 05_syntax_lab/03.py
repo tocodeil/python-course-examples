b269520a1c71dcaea3f3d97dd6e3380c5cd5395a
ygolan99 <yariv.golan@verint.com>
2016-11-30 15:33:34 +0200

    Solution for: 05_syntax_lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -5,4 +5,17 @@ and prints the sum of its digits.
 For example if the number was: 2345
 the result should be: 14.
 """
+total=0
+#sum=0
+from random import randint
+for i in range(1, 2):
 
+     numbers = randint(1,1000)
+     print " the random number is:", numbers
+     orech= len(str(numbers))
+for y in range (len(str(numbers))):
+      sum = numbers % 10
+      total+=sum
+      numbers /= 10
+
+print "the sum of the digits is:", total
