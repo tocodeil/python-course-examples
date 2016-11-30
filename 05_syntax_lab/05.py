2ca49a81bbc0d8e4e59add8ed50a1a06ab45bc37
elibrich <elena.librich@verint.com>
2016-11-30 20:28:46 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -2,4 +2,13 @@
 Write a program that generates random integers in a loop
 until it finds a number that is divisible by: 7, 13, and 15.
 """
+from random import randint
 
+found = False
+
+while(found != True):
+    num = randint(1,1000000)
+    if((num % 7 == 0) and (num % 13 == 0) and (num % 15 == 0)):
+        found = True
+
+print "num",num
