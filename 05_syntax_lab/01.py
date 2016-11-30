74792a0183891e200ca076cd19ea39c908e90565
shlomiiss <shlomiiss@gmail.com>
2016-11-30 09:28:44 +0200

    Solution for: 05_syntax_lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,18 +1,12 @@
 """
  This code is getting 10 numbers and printing out the biggest.
 """
-
-number = 0
-bignumber = 0 
-oldnumber = 0
-i = 0
-print ("Enter 10 decimal numbers") 
-for i in range (1,10):
-    number = int(raw_input())
+number = float('-inf')
+bignumber = float('-inf')
+print ("Enter 10 decimal numbers")
+for _ in range(10):
+    number = float(raw_input())
     if (number > bignumber ) :
-      bignumber = number
-    
-print ("The final biggest number is", bignumber)
-        
+         bignumber = number 
+print ("Max number = %f" % bignumber)
 
-        
