1cbe142a50e2295fe82ab814812e1bc2ea28b819
LiatKotigaro <liat.kotigaro@verint.com>
2016-11-30 23:54:36 +0200

    Solution for: 06_modules_lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -9,3 +9,25 @@ its name to the user.
 - Take threshold and path as command line arguments
 """
 
+import sys
+import os
+
+if len(sys.argv) != 3:
+    print "Usage: %s <dirpath> <threshold>" % sys.argv[0]
+    sys.exit(1)
+
+(_, dirpath,threshold) = sys.argv
+
+
+for root,dirs,files in os.walk(dirpath):
+    for name in files:
+      fullname = "%s\\%s" % (root,name)
+      filesize = os.stat(fullname).st_size
+      if float(filesize) > float(threshold):
+        print "%s size is %d. " % (fullname,filesize),
+        print r"Do you want to delete it (y\n)?",
+        answer = raw_input()
+        if answer == 'y':
+          os.remove(fullname)
+        elif answer == 'Y':
+          os.remove(fullname)
