99daca8ce82809dbce0de41b698628958576457a
igalzzz <igal22@gmail.com>
2016-12-01 09:36:14 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -7,3 +7,16 @@ For example if the numbers were 4 and 6,
 program should print 12.
 """
 
+import random
+randA = random.randint(1,100)
+randB = random.randint(1,100)
+print ("First random = %d, Second random = %d" % (randA, randB))
+if randA >= randB:
+	number = randA
+else:
+	number = randB
+while True:
+	if ((number % randA == 0 ) and (number % randB ==0)):
+		break
+	number +=1
+print (number)
