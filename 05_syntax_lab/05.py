9f0702a6f9f5bc7e44b52bb7290220b591766f5f
igalzzz <igal22@gmail.com>
2016-12-01 20:20:11 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -6,7 +6,6 @@ until it finds a number that is divisible by: 7, 13, and 15.
 import random
 import sys
 
-number = 0
 while True:
 	number = random.randint(0, sys.maxint)
 	if (number % 7 == 0) and (number % 13 == 0) and (number % 15 == 0):
