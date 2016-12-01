0aeb6acd28c5d49d40800005e222571f75cfe999
Balinbal <inbalmiezels@gmail.com>
2016-12-01 13:34:04 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -6,7 +6,7 @@ until it finds a number that is divisible by: 7, 13, and 15.
 import random
 
 while True:
-  num=random.randrange(1000000)
+  num = random.randrange(1000000)
   if num % 7 == 0 and num % 13 == 0 and num % 15 == 0:
     break
 print "The number is:" ,num   
