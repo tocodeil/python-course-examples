7ad2c553d8da129574ab8c09888229951fed2c20
ItayElbaz <itayelbaz7@gmail.com>
2016-12-01 12:03:10 +0200

    Solution for: 10_datastructures_lab/04.py

--- 10_datastructures_lab/04.py
+++ 10_datastructures_lab/04.py
@@ -4,4 +4,32 @@ Program takes a path to a words file,
 reads the file and searches for words with the same letters.
 All words with the same letters should be printed together
 """
+"""
+dicn = {}
+with open(r'.\words') as words:
+    for word in words:
+        dicn[word.strip('\n')] = set(word.strip('\n'))
+
+"""
+
+word_list = []
+
+with open(r'.\words') as words:
+    for word in words:
+        word_list.append(word.strip('\n'))
+
+
+for word in word_list:
+    for another_word in word_list:
+        if set(word) == set(another_word) and word != another_word:
+                print word , another_word
+                word_list.remove(word),word_list.remove(another_word)
+
+for rest in word_list:
+    print rest
+
+
+
+
+
 
