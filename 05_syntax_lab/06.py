447a268f6c801509e9ffb440b6dac0c647bac0fe
anatshapira <anatshapira8@gmail.com>
2016-12-01 22:45:24 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,20 @@
 """
-Write a program that generates 2 random numbers
-and calculates their least common multiple,
-that is the smallest number that is divisible
-by both.
-For example if the numbers were 4 and 6,
-program should print 12.
+syntax_ex06.py
+
 """
 
+from random import randint 
+
+
+rand_num1 = randint(1, 10)
+rand_num2 = randint(1, 10)
+min_multiple = rand_num1*rand_num2
+i=min_multiple
+print " for those two numbers: ",rand_num1, ", " ,rand_num2
+
+while i>=rand_num1 and i>=rand_num2:
+    if i%rand_num1 + i%rand_num2 == 0:
+        min_multiple = i
+    i-=1
+
+print " the smallest multiple is: ",min_multiple
