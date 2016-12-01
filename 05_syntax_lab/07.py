f677ddb7eec54c780ff833a902a471dc460ce4e4
Balinbal <inbalmiezels@gmail.com>
2016-12-01 13:34:06 +0200

    Solution for: 05_syntax_lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -6,17 +6,15 @@ After each guess the program prints a hint "too large" or "too small" to the use
 """
 import random
 
-num=random.randrange(100)
-print "Please enter your guess:"
-UserNum=int(raw_input())
+num = random.randrange(100)
+
 while True:
+  print "Please enter your guess:"
+  UserNum = int(raw_input())
   if num == UserNum:
     print "Excellent you are right, the number is:" ,num
     break
   elif num > UserNum:
-    print "Too small"
-  else :
-    print "Too big"
-  print "Please enter new guess:"
-  UserNum=int(raw_input())
-
+	  print "Too small"
+  else :  
+	  print "Too big"
