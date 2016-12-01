422c3cfabdc294b8ff0174ce808a9c04780377fa
Balinbal <inbalmiezels@gmail.com>
2016-12-01 13:34:08 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -3,12 +3,12 @@ The program generates 7 random integers
 between 1 and 100, and prints their sum.
 If the sum is divisible by 7, the program print the word "Boom".
 """
-sum=0
+sum = 0
 import random
 
 for num in range(7):
-  num=random.randint(1,100)
-  sum=sum+num
+  num = random.randint(1,100)
+  sum = sum + num
 
 print sum
 if sum % 7 == 0:
