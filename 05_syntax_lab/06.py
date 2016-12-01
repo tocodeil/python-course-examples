379a634cdcaacccd348abd74cae750873b2b3ec3
igalzzz <igal22@gmail.com>
2016-12-01 20:20:07 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -8,15 +8,22 @@ program should print 12.
 """
 
 import random
+import sys
+
 randA = random.randint(1,100)
 randB = random.randint(1,100)
 print ("First random = %d, Second random = %d" % (randA, randB))
 if randA >= randB:
-	number = randA
-else:
 	number = randB
+	tempNum = randB
+else:
+	number = randA
+	tempNum = randA
 while True:
 	if ((number % randA == 0 ) and (number % randB ==0)):
 		break
-	number +=1
+	number += tempNum
+	if (number > (sys.maxint-1)):
+		print ("Coudn't find the least common multiple  for First random = %d, Second random = %d in the limitation of max integer" % (randA, randB))
+		sys.exit(1)
 print (number)
