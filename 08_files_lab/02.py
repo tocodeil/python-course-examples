f93a0312e772b6f16f132e33190cd49767e78146
Balinbal <inbalmiezels@gmail.com>
2016-12-03 22:00:44 +0200

    Solution for: 08_files_lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,26 +1,30 @@
 """
-Write a program that takes 3 names. The first two
+The program takes 3 names. The first two
 are existing file names and the last is a new file name/
 The program should write the lines from the first two 
-files interwinded into the output file. So if we have
-
-a.txt with
-a1
-a2
-
-and b.txt with
-b1
-b2
-
-then running
-
-python 02.py a.txt b.txt c.txt
-will result in the creation of c.txt with the following content:
-
-a1
-b1
-a2
-b2
+files interwinded into the output file.
 
 """
 
+import sys
+import itertools
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+file3new = sys.argv[3]
+
+with open (file3new ,'w') as fin:
+	with open (file1 ,'r') as fout:
+		line1 = fout.readlines()
+		line1[-1] += '\n'
+	with open (file2,'r') as foo:
+		line2 = foo.readlines()
+	result = itertools.izip_longest(line1,line2,fillvalue='')
+	l = list(result)
+	text = ''
+	for pair in l:
+		text += pair[0] + pair[1]
+	fin.write(text)
+	print text
+	
+	
