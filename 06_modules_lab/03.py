9341763b84a9af2f15ee54001f036b555f8a3694
Balinbal <inbalmiezels@gmail.com>
2016-12-03 10:54:31 +0200

    Solution for: 06_modules_lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,24 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
+""" The program searches current working directory
+for files larger than 1MB. Every time you find such a file the program print
 its name to the user.
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
-
-- Take threshold and path as command line arguments
 """
 
+import os
+import sys
+
+path = sys.argv[1]
+MinSize = int(sys.argv[2])
+for root,dirs,files in os.walk(path):
+	for name in files:
+		full_path = root + '\\' + name
+		statinfo = os.stat(full_path)
+		if statinfo.st_size >= MinSize:
+			print name, '\n' , "Do you want to delete the file? Enter yes or no"
+			UserAnswer = raw_input()
+			if (UserAnswer).lower() == 'yes':
+				os.remove(full_path)
+				print "Your file deleted successfully"
+			elif (UserAnswer).lower() == 'no':
+				pass
+			
