3570b77a7eb6f0c9037c6d45ffe36e97365a8752
mesika <yaniv.mesika@gmail.com>
2016-12-03 23:14:21 +0800

    Solution for: 10_datastructures_lab/04.py

--- 10_datastructures_lab/04.py
+++ 10_datastructures_lab/04.py
@@ -4,4 +4,19 @@ Program takes a path to a words file,
 reads the file and searches for words with the same letters.
 All words with the same letters should be printed together
 """
+import collections
 
+wordDic = collections.defaultdict(list)
+
+with open("words", "r") as fin:
+    for line in fin:
+        stripped = line.strip()
+        if not stripped:
+            continue
+
+        sortedWord = sorted(stripped)
+        key = "".join(sortedWord)
+        wordDic[key].append(stripped)
+
+for words in wordDic.values():
+    print " ".join(words)
