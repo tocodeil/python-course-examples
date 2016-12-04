f46b4de50d63e1dec9fb786fe413cbd2a712e93c
Balinbal <inbalmiezels@gmail.com>
2016-12-05 00:13:43 +0200

    Solution for: 10_datastructures_lab/03.py

--- 10_datastructures_lab/03.py
+++ 10_datastructures_lab/03.py
@@ -1,7 +1,27 @@
 """
 A file named hosts holds hostnames and IP addresses
 in format: hostname=ip
-Write a program that reads the file and takes
+The program reads the file and takes
 a list of hostnames in sys.argv
-Program should print the IP addresses of the hosts requested
+Program print the IP addresses of the hosts requested
 """
+
+import sys
+
+list = set(sys.argv[1:])
+filename = 'hosts'
+hostslist = []
+name = []
+ip =[]
+print list
+with open (filename,'r') as f:
+	for line in f:
+		hostslist.append(line.strip().split('='))
+	for pair in hostslist:
+		if len(pair) > 1:
+			name += [pair[0]] 
+			ip += [pair[1]]
+for l in list:
+	for n in range(len(name)):
+		if l ==  name[n]:
+			print name[n] ,'=',ip[n]
