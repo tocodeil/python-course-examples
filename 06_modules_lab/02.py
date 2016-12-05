c73bdbe3aa7976460cb8767ef88e6306b35155bf
shlomiiss <shlomiiss@gmail.com>
2016-12-05 20:01:38 +0200

    Solution for: 06_modules_lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,13 @@
+ 
 """ Write a program that reads 2 numbers from sys.argv
 and prints their sum.
 """
 
+import sys
+userinput1 = sys.argv[1]
+userinput2 = sys.argv[2]
+print ("Sum is %d" % (int(userinput1)+int(userinput2)))
+  
+
+
+   
