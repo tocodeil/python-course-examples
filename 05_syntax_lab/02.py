dae0a4ec7d340e3030f6fdfbe8a8a8fbf2ae06b9
Chagit171 <Chagit171@gmail.com>
2016-12-05 11:39:34 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -11,12 +11,12 @@ If the sum is divisible by 7, also print the word "Boom".
 
 
 from random import randint
-sum=0
-for x in range(0,7):
-    random_number=randint(1,100)
-    sum+=random_number
+sum = 0
+for x in range(7):
+    random_number = randint(1,100)
+    sum+= random_number
 print "The sum is: " , sum
-if sum%7==0:
+if sum%7 == 0:
     print "Boom"
 
     
