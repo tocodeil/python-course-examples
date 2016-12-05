ce7f41f886da2b32f03cc522c05571cfba6f1d1f
ItayElbaz <itayelbaz7@gmail.com>
2016-12-05 10:35:05 +0200

    Update 02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -6,5 +6,5 @@ If wrong types were passed in, raise an exception
 """
 
 def take_string_and_number(s,n):
-    if type(s) is not int: raise Exception("Required an 'int' for the first parameter")
-    if type(n) is not str: raise Exception("Required a 'str' for the first parameter")
+    if type(s) is not str: raise Exception("Required 'str' for the first parameter")
+    if type(n) is not int: raise Exception("Required 'int' for the first parameter")
