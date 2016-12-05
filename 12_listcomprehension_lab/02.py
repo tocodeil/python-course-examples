726824911d559e13d625d9947c20b03ae3a56418
gbarzilay <galia.barzilay@verint.com>
2016-12-05 15:59:47 +0200

    Solution for: 12_listcomprehension_lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -3,4 +3,10 @@ Write a python program that takes two words
 as sys.argv and prints only the letters
 common to both
 """
+import sys
+
+letters1 = set(sys.argv[1])
+letters2 = set(sys.argv[2])
+
+print [x for x in letters1 if x in letters2]
 
