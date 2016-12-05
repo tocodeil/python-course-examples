e6ce9970bdd9a364f23b590fc5c86d08bd607b09
LiatKotigaro <liat.kotigaro@verint.com>
2016-12-05 23:49:13 +0200

    Solution for: 08_files_lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -23,4 +23,26 @@ a2
 b2
 
 """
+import sys
+import itertools
 
+if len(sys.argv) != 4:
+    print "Usage: %s <infile1> <infile2> <outfile>" % sys.argv[0]
+    sys.exit(1)
+
+(_, infile1,infile2,outfile) = sys.argv
+
+bufsize = 20
+lines1 = []
+lines2 = []
+with open(outfile, "w") as fout:
+    with open(infile1, "r") as fin1:
+        lines1 = [line.rstrip('\n') for line in fin1]
+    with open(infile2, "r") as fin2:
+        lines2 = [line.rstrip('\n') for line in fin2]
+    combinedLines = list(itertools.izip_longest(lines1, lines2, fillvalue=''))
+    for line in combinedLines:
+        if len(line[0]) > 0:
+            fout.write(line[0] + '\n')
+        if len(line[1]) > 0:
+            fout.write(line[1] + '\n')
