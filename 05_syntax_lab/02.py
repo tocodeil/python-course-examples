0b2c91163b8c169ae7d9bc814d24dbf1284dcc61
ohady75 <ohad.yanai@verint.com>
2016-12-05 23:16:40 +0200

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -3,4 +3,14 @@ Write a Python program that generates 7 random integers
 between 1 and 100, and prints their sum.
 If the sum is divisible by 7, also print the word "Boom".
 """
+print "good day Yo"
+import random
 
+summ =0
+for n in range (7):
+   summ =summ + random.randint (0,100)  
+
+print summ
+if summ%7==0:
+    print "Boom"
+    
