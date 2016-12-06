ccdc234361a2a1b7d55d52fcbc613663b837ddbd
anatshapira <anatshapira8@gmail.com>
2016-12-06 20:56:08 +0200

    Solution for: 05_syntax_lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -10,8 +10,13 @@ while i < 10:
 
     print "Please enter a number:"
     new_num = raw_input()
+    while not new_num.isdigit():
+        print "invalid input, try again... or cancel"
+        new_num = raw_input()
+
     if new_num > max_num:
         max_num = new_num
     i+=1
+
 
 print "your biggest number enterd so far is: " , max_num
