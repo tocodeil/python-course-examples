ae5b45f4ae66652aa7702130fa53bc0c29e37a84
Chagit171 <Chagit171@gmail.com>
2016-12-06 14:08:43 +0200

    Solution for: 12_listcomprehension_lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -4,3 +4,9 @@ as sys.argv and prints only the letters
 common to both
 """
 
+
+word1 = raw_input("Insert a number: ")
+word2 = raw_input("Insert another number: ")
+
+common =list(set( [ c1 for c1 in word1 for c2 in word2 if c1==c2]))
+print common
