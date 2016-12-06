8bde7f1fb25520c76c1bbbb5baa1b280ad5f4a6d
eranshaked <eran.shaked@verint.com>
2016-12-06 22:27:25 +0200

    Solution for: 05_syntax_lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -5,4 +5,12 @@ After the user typed in an empty line,
 print all previously entered lines in reverse
 order (from last to first).
 """
+lines = ""
+print "please type ... only ENTER stop "
+line = raw_input()
 
+while len(line) > 0:
+    lines += '\n'+line
+    line = raw_input()
+for i in reversed(lines.split('\n' )):
+ print i
