a95d86ab9712ffe93fc8997aa78e31ac21466a5a
User1978 <User1978@users.noreply.github.com>
2016-12-07 08:12:05 +0200

    Solution for: 10_datastructures_lab/04.py

--- 10_datastructures_lab/04.py
+++ 10_datastructures_lab/04.py
@@ -5,3 +5,21 @@ reads the file and searches for words with the same letters.
 All words with the same letters should be printed together
 """
 
+from collections import defaultdict
+
+anagrams = defaultdict(set)
+
+with open('words', 'r') as dictionary:
+   for word in dictionary:
+      word = word.rstrip()
+      anagrams[''.join(sorted(word))].add(word)
+
+#print anagrams
+
+for key, value in anagrams.items():
+    #print key
+    for pair in value:
+        #print pair
+        print key, "-->", pair
+
+#print anagrams.items()
