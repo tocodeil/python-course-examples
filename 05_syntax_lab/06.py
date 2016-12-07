20d24e2a3903f28a2435f38b1fba69bbafd19f00
deftdot <nickkaplan1988@gmail.com>
2016-12-07 15:10:47 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,7 @@
-"""
-Write a program that generates 2 random numbers
-and calculates their least common multiple,
-that is the smallest number that is divisible
-by both.
-For example if the numbers were 4 and 6,
-program should print 12.
-"""
-
+from random import randint
+rnd1=randint(1,11)
+rnd2=randint(1,11)
+for i in range(1,rnd1*rnd2+1,1):
+    if i % rnd1 == 0 and i % rnd2 == 0:
+        print "The 1st random number is {random1} \n The 2nd random number is {random2} \n The smallest devided number by both is {smallestDev}".format(random1 = rnd1,random2 = rnd2,smallestDev = i)
+        break
