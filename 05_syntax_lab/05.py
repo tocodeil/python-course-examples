24eaae10bfafff41c0c0927a3b4e59d4bd2f7821
deftdot <nickkaplan1988@gmail.com>
2016-12-07 15:10:46 +0200

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,10 @@
-"""
-Write a program that generates random integers in a loop
-until it finds a number that is divisible by: 7, 13, and 15.
-"""
+from random import randint
+rnd=0
+while True:
+    rnd = randint(1,1000000001)
+    if rnd % 15 == 0 and rnd % 13 == 0 and rnd % 7 == 0:
+        print "The random number is {random}, and it devided by 15,13 and 7.".format(random = rnd)
+        break
+    else: continue
+
 
