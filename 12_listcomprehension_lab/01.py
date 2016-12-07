d4e0c02b70911881221c3edb782ddaeef8b242f0
Lionellionelel <lionelel@gmail.com>
2016-12-07 17:33:23 +0200

    Solution for: 12_listcomprehension_lab/01.py

--- 12_listcomprehension_lab/01.py
+++ 12_listcomprehension_lab/01.py
@@ -1,8 +1,8 @@
-"""
-Use range() and list comprehension to get
-the list of all lowercase english letters
-Hint: look for chr() and ord()
-"""
+import string
 
 
+
+#all_letters = string.ascii_letters
+for i in range(97, 123):
+    print chr(i).lower()
 
