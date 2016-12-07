61f62bfdebf0868882076be2c339481d9266e41a
nomzg <nomi.guthmann@gmail.com>
2016-12-07 16:16:37 +0200

    Solution for: 05_syntax_lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -4,4 +4,21 @@ After each guess print a hint "too large" or "too small" to the user.
 Bonus: To make things interesting, the program should cheat once in a white
 """
 
+from random import randint
 
+num2Guess = randint(1,100)
+
+print("Guess a number!")
+
+guess = int(raw_input())
+fountit = False
+while not fountIt:
+	if guess > num2Guess:
+		print("too large")
+		guess = int(raw_input())
+	elif guess < num2Guess:
+		print("too small")
+		guess = int(raw_input())
+	elif guess == num2Guess:
+		print("found it!")
+		fountit = True
