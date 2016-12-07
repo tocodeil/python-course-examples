db10b0c8eb80ea11f013813b2831b152d40c093d
Chagit171 <Chagit171@gmail.com>
2016-12-07 10:45:07 +0200

    Solution for: 12_listcomprehension_lab/01.py

--- 12_listcomprehension_lab/01.py
+++ 12_listcomprehension_lab/01.py
@@ -4,7 +4,7 @@ the list of all lowercase english letters
 Hint: look for chr() and ord()
 """
 
-ascii_val = range(97,123)
+ascii_val = range(ord('a'), ord('z')+1)
 print ascii_val
 print [ chr(x) for x in ascii_val ]
 
