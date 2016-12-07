40ea077b3c90b125c74c9fad63ffc663f7567509
User1978 <User1978@users.noreply.github.com>
2016-12-07 08:11:44 +0200

    Solution for: 10_datastructures_lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -4,6 +4,12 @@ and print only the grades that
 are above average
 """
 
+
 grades = [99, 90, 15, 28, 38, 44, 50, 81, 79, 60, 99, 90, 15, 28, 38, 44, 50,
         81, 79, 60 ]
 
+mean = float(sum(grades)) / max(len(grades), 1)
+
+for grade in grades:
+    if grade >= mean:
+        print grade
