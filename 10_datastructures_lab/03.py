05c3d526d64fa3ed7e93245ddaa9e7f1450a5beb
Lionellionelel <lionelel@gmail.com>
2016-12-07 15:02:16 +0200

    Solution for: 10_datastructures_lab/03.py

--- 10_datastructures_lab/03.py
+++ 10_datastructures_lab/03.py
@@ -1,7 +1,15 @@
-"""
-A file named hosts holds hostnames and IP addresses
-in format: hostname=ip
-Write a program that reads the file and takes
-a list of hostnames in sys.argv
-Program should print the IP addresses of the hosts requested
-"""
+import sys
+#print sys.argv[1:]
+
+
+pc_list = set(sys.argv[1:])
+dictionary = {}
+
+with open(r"C:\Temp\Hosts", "r") as rows:
+    for row in rows:
+        dictionary.setdefault(row.split("=")[0], row.split("=")[1].strip('\n'))
+
+#print dictionary
+
+for pc in pc_list:
+    print "%s ==> %s" % (pc, dictionary.get(pc, 'PC was not identified in hosts file'))
