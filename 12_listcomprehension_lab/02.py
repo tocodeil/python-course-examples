e2a37b04f50110a8076946e629a37b04c3e26824
Lionellionelel <lionelel@gmail.com>
2016-12-07 17:33:25 +0200

    Solution for: 12_listcomprehension_lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,21 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
 
+from collections import Counter
+
+word1 = raw_input("Enter a word")
+word2 = raw_input("Enter a word again")
+
+
+lst_word1 = Counter(word1)
+lst_word2 = Counter(word2)
+
+if len(lst_word1) >= len(lst_word2):
+    first = lst_word1
+    second = lst_word2
+else:
+    first = lst_word2
+    second = lst_word1
+
+for i in first:
+    for j in second:
+        if i == j:
+            print i
