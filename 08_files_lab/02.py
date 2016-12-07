542e7f83bc5984c2d1c88575348ede6744cf83e8
Chagit171 <Chagit171@gmail.com>
2016-12-07 10:59:43 +0200

    Solution for: 08_files_lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -24,3 +24,19 @@ b2
 
 """
 
+import sys
+import itertools
+
+"""(src1, src2,dst) = sys.argv[2:]"""
+
+src1 = "C:\\Users\\cchaimov\\Documents\\Ex3_python\\1.txt"
+src2 = "C:\\Users\\cchaimov\\Documents\\Ex3_python\\2.txt"
+dst  = "C:\\Users\\cchaimov\\Documents\\Ex3_python\\3.txt"
+                
+with open(src1, "r") as fout1, open(src2, "r") as fout2:
+        with open(dst, "w") as fin:
+            lines = itertools.izip_longest(fout1,fout2,fillvalue = "")
+            for i in lines:
+                fin.write(str(i[0]))
+                fin.write(str(i[1]))
+            
