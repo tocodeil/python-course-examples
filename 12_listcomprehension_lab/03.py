d6c245db29467c7a83b0d531aa50caa9f48ee527
Chagit171 <Chagit171@gmail.com>
2016-12-07 10:45:15 +0200

    Solution for: 12_listcomprehension_lab/03.py

--- 12_listcomprehension_lab/03.py
+++ 12_listcomprehension_lab/03.py
@@ -5,10 +5,7 @@ of 3 lowercase characters: aaa, aab, aac, ..., zzz
 
 import string
 
-chr_lst1 = list(string.ascii_lowercase)
+chr_lst = list(string.ascii_lowercase)
 
-chr_lst2 = list(string.ascii_lowercase)
+print list(set( [ c1+c2+c3 for c1 in chr_lst for c2 in chr_lst for c3 in chr_lst ]))
 
-chr_lst3 = list(string.ascii_lowercase)
-
-print list(set( [ c1+c2+c3   for c1 in chr_lst1 for c2 in chr_lst2 for c3 in chr_lst3]))
