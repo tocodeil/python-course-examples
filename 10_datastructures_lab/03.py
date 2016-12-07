2da75c3d6747716f1b3e33ce000dbdd090f02181
LiatKotigaro <liat.kotigaro@verint.com>
2016-12-07 21:09:31 +0200

    Solution for: 10_datastructures_lab/03.py

--- 10_datastructures_lab/03.py
+++ 10_datastructures_lab/03.py
@@ -5,3 +5,30 @@ Write a program that reads the file and takes
 a list of hostnames in sys.argv
 Program should print the IP addresses of the hosts requested
 """
+import sys
+
+#Get input
+hosts_count = len(sys.argv) - 1
+
+if hosts_count < 1:
+    print "Usage: %s <host1> <host2> ... <host n>" % sys.argv[0]
+    sys.exit(1)
+
+
+#Read hosts file to dictionary
+hosts_dictionary = {}
+with open("hosts", "r") as fhosts:
+    for hostline in fhosts:
+        hostparams = hostline.split("=")
+        if len(hostparams) < 2:
+            continue
+        hosts_dictionary[hostparams[0]] = hostparams[1].strip("\n")
+
+#Print host ip by host name
+for i in range(1,hosts_count+1):
+    if hosts_dictionary.get(sys.argv[i]) != None:
+        print "%s ==> %s" % (sys.argv[i], hosts_dictionary[sys.argv[i]])
+    else:
+        print "%s ==> %s" % (sys.argv[i], "Error: No IP address was set to this host")
+ 
+
