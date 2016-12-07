ac7cf46bdbb0d75c2a12cd2b7ef49f1ac62e7448
Lionellionelel <lionelel@gmail.com>
2016-12-07 15:43:54 +0200

    Solution for: 10_datastructures_lab/04.py

--- 10_datastructures_lab/04.py
+++ 10_datastructures_lab/04.py
@@ -1,7 +1,19 @@
-"""
-Write a program that finds anagrams in a words file
-Program takes a path to a words file, 
-reads the file and searches for words with the same letters.
-All words with the same letters should be printed together
-"""
+from collections import  Counter
+
+
+lst = []
+with open(r"C:\Temp\anagram", "r") as rows:
+    for row in rows:
+        lst += [row.strip('\n')]
+
+
+new_list = []
+for k in lst:
+    for i in range(1, len(lst)):
+        if Counter(k) == Counter(lst[i]) and lst[i] not in new_list and k is not lst[i]:
+            new_list += [lst[i]]
+            print "%s %s" % (k, lst[i])
+
+
+
 
