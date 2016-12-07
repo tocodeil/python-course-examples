376e37fed49c39c4b1f5dcfd4b620fb9dd75b7ad
shlomiiss <shlomiiss@gmail.com>
2016-12-07 17:32:12 +0200

    Solution for: 08_files_lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,34 +1,45 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one.
+""" 
+ Write a program that takes 2 file names 
+ and appends the second file's contents to 
+the end of the first one. 
+  
+ So if file 'a.txt' has: 
+  
+ a1 
+ a2 
+  
+ and file 'b.txt' has 
+  
+ b2 
+ b2 
+ b3 
+  
+ then after running 
+    python 01.py a.txt b.txt 
+ a.txt will have 
+  
+ a1 
+ a2 
+ b1 
+ b2 
+ b3 
+ and b.txt will have 
+  
+ b1 
+ b2 
+ b3 
+  
+ """ 
 
-So if file 'a.txt' has:
 
-a1
-a2
+import sys
+import os
+(srcfile, dstfile) = sys.argv[1:]
 
-and file 'b.txt' has
 
-b2
-b2
-b3
-
-then after running
-   python 01.py a.txt b.txt
-a.txt will have
-
-a1
-a2
-b1
-b2
-b3
-
-and b.txt will have
-
-b1
-b2
-b3
-
-"""
+with open(dstfile, "a") as fout:
+ with open(srcfile, "r") as fin:
+  for line in fin:
+     fout.write(line)
+  
 
