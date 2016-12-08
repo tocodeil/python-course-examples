10295789b63cdf2451ee74ecf8fb17f4863a3144
elhi <elhanan.rahamim@verint.com>
2016-12-08 07:10:02 +0200

    Solution for: 08_files_lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,26 +1,10 @@
+""" Write a program that reads 2 numbers from sys.argv
+and prints their sum.
 """
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name/
-The program should write the lines from the first two 
-files interwinded into the output file. So if we have
 
-a.txt with
-a1
-a2
-
-and b.txt with
-b1
-b2
-
-then running
-
-python 02.py a.txt b.txt c.txt
-will result in the creation of c.txt with the following content:
-
-a1
-b1
-a2
-b2
-
-"""
+import sys
+first = int(sys.argv[1])
+second = int(sys.argv[2])
+sum = first + second
+print sum
 
