ec01cb831fb4307e882d53849fd1b15a3f5497ee
deftdot <nickkaplan1988@gmail.com>
2016-12-08 10:41:55 +0200

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -5,4 +5,3 @@ for i in range(1,rnd1*rnd2+1,1):
     if i % rnd1 == 0 and i % rnd2 == 0:
         print "The 1st random number is {random1} \n The 2nd random number is {random2} \n The smallest devided number by both is {smallestDev}".format(random1 = rnd1,random2 = rnd2,smallestDev = i)
         break
-        
