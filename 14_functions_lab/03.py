371284aaa772f4cf0d52364a1d60a1e73b528de0
igalzzz <igal22@gmail.com>
2016-12-08 18:12:32 +0200

    Solution for: 14_functions_lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -4,8 +4,12 @@ of the 10th digit from all arguments passed to it
 """
 
 
-def sum_tens():
-    pass
+def sum_tens(*args):
+	summery = 0
+	for n in args:
+		if type(n) is int: 
+			summery += int(str(n)[-2:-1]) 
+	return summery
 
 
-# print sum_tens(120, 140, 1123)
+#print sum_tens(120, 140, 1123)
