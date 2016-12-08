e55b323b8d0e969a413a594f77e0e88f8865f09d
Balinbal <inbalmiezels@gmail.com>
2016-12-08 22:39:02 +0200

    Solution for: 12_listcomprehension_lab/03.py

--- 12_listcomprehension_lab/03.py
+++ 12_listcomprehension_lab/03.py
@@ -3,3 +3,7 @@ Find and print all possible sequences
 of 3 lowercase characters: aaa, aab, aac, ..., zzz
 """
 
+l = range(97,123)
+digits = [chr(x) for x in l]
+rez = [x + y + z for x in digits for y in digits for z in digits]
+print rez
