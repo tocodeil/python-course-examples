76b4a77f3d5ec0aec51f300550c546766a6cc7e9
Balinbal <inbalmiezels@gmail.com>
2016-12-08 22:24:05 +0200

    Solution for: 12_listcomprehension_lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -4,3 +4,11 @@ as sys.argv and prints only the letters
 common to both
 """
 
+import sys
+import string
+
+word1 = sys.argv[1]
+word2 = sys.argv[2]
+
+similiar = [digit for digit in word1 if string.find(word2,digit) != -1]
+print similiar
