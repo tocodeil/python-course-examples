923852cc3f5ac536308b8d9615b5807f9d6bd912
elhi <elhanan.rahamim@verint.com>
2016-12-08 09:33:35 +0200

    Solution for: 08_files_lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,23 +1,45 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times.
-
-For example if program was started as:
-    python 01.py 5
-
-It should print:
-    Hello Python
-    Hello Python
-    Hello Python
-    Hello Python
-    Hello Python
 """
+Write a program that takes 2 file names
+and appends the second file's contents to
+the end of the first one.
 
+So if file 'a.txt' has:
+
+a1
+a2
+
+and file 'b.txt' has
+
+b2
+b2
+b3
+
+then after running
+   python 01.py a.txt b.txt
+a.txt will have
+
+a1
+a2
+b1
+b2
+b3
+
+and b.txt will have
+
+b1
+b2
+b3
+
+"""
 import sys
-for _ in range(int(sys.argv[1])) :
-        print "Hello Python"
+#Path = sys.argv[1]
+First_file = sys.argv[1]
+Second_file = sys.argv[2]
 
-
+with open(First_file, "r") as fin:
+    with open(Second_file, "a") as fout:
+        for line in fin:
+            fout.write(line)
 
 
 
