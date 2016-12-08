3214e1018e728b97c1f2835c243a45b4f46cd30e
shlomiiss <shlomiiss@gmail.com>
2016-12-08 15:20:58 +0200

    Solution for: 06_modules_lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,13 +1,11 @@
- 
 """ Write a program that reads 2 numbers from sys.argv
 and prints their sum.
 """
 
 import sys
-userinput1 = sys.argv[1]
-userinput2 = sys.argv[2]
-print ("Sum is %d" % (int(userinput1)+int(userinput2)))
-  
 
+(x ,y) = sys.argv[1:]
 
-   
+x1= float(x)
+x2= float(y)
+print ("The sum of the two number is %f" % float(x1+x2))
