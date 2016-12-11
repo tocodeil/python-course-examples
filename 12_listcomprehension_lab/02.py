c2c62dc975165c420b948099e054fa2f8eb0f5a4
shlomiiss <shlomiiss@gmail.com>
2016-12-11 17:28:22 +0200

    Solution for: 12_listcomprehension_lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,17 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
 
+""" 
+ Write a python program that takes two words 
+ as sys.argv and prints only the letters 
+ common to both 
+ """ 
+
+#x.lower() for x in ["A","B","C"]]
+import sys
+print ("please enter two words")
+matchedletters=[]
+uniguechars=[]
+(word1 ,word2) = sys.argv[1:]
+
+matchedletters  += [char for char in word1 if (char in word2)]
+uniguechars=set(matchedletters)
+print uniguechars
