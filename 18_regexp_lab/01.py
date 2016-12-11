190d0e5c686f3e7a08580bd8e0e3b57cefef7aee
gbarzilay <galia.barzilay@verint.com>
2016-12-11 11:33:36 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -10,3 +10,28 @@ Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
 
+import re
+import sys
+
+if len(sys.argv) != 3:
+    print "usage %s <filename> <key>" % sys.argv[0]
+    sys.exit()
+
+def iscomment(line):
+    return re.search('^#', line)
+
+def findValue(key, line):
+    regexp = r'%s\s*=\s*(\w*)' % sys.argv[2]
+    m = re.search(regexp, line)
+    if m: return m.group(1)
+    else: return None
+
+with open(sys.argv[1], 'r') as fin:
+    for line in fin:
+        if not iscomment(line):
+           value = findValue(sys.argv[2], line)
+           if value != None:
+               print value
+               break
+
+
