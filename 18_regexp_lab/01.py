242903797d33cde11580dfabaa4199a82cfe5f4c
Balinbal <inbalmiezels@gmail.com>
2016-12-12 11:10:32 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -9,4 +9,16 @@ Each line in the file can either be:
 Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
+import re
+
+print "Please Enter file and key:"
+file = raw_input()
+key = raw_input()
+
+with open (file , 'r') as foo:
+	l= foo.readlines()
+	for line in l:
+		if re.search((r'^' + key),line):
+			print line.split('=')[1].lstrip()
+			
 
