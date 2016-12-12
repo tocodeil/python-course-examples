678f74b8aa0b194ee229ba7661a88467af00fcb0
shlomiiss <shlomiiss@gmail.com>
2016-12-12 17:54:06 +0200

    Solution for: 06_modules_lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,22 +1,22 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times.
-For example if program was started as:
-    python 01.py 5
-It should print:
-    Hello Python
-    Hello Python
-    Hello Python
-    Hello Python
-    Hello Python
-"""
-import sys
-numberprint = int(sys.argv[1])
-index = 1
-for index in range(numberprint):
-   print 'Hello Python'
-   index=+1
-   
-   
+""" Write a program that takes a count 
+ from sys.argv import and prints "Hello Python" 
+ count times. 
+  
+ For example if program was started as: 
+     python 01.py 5 
+  
+ It should print: 
+     Hello Python 
+     Hello Python 
+     Hello Python 
+     Hello Python 
+     Hello Python 
+ """ 
 
 
+
+import sys  
+numberprint = int(sys.argv[1])  
+for index in range(numberprint):  
+ print 'Hello Python'  
+ 
