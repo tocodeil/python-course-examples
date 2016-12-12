0cdcef946a3b9a07816bdd7fa35787dc23e0abe1
ItayElbaz <itayelbaz7@gmail.com>
2016-12-12 13:34:35 +0200

    Solution for: 18_regexp_lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -4,3 +4,19 @@ Write a function to_underscore(text) that returns the text with
 underscores between words
 """
 
+import re
+
+text = "hello_big_big_world"
+text2 = "helloBigWorld"
+
+def camel_case(text):
+    space_first = re.sub('_',' ',text)
+    upper_first = re.sub(r'\s\b([a-z])', lambda m: m.group(1).upper(), space_first)
+    no_sapce = re.sub(' ','',upper_first)
+    print no_sapce
+
+def undo_camel_case(text):
+    add_space = re.sub(r'([A-Z])',lambda m: " " + m.group(1).lower(),text )
+    underscore = re.sub(' ','_',add_space)
+    print underscore
+
