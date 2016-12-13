895ca6bd1c00f0c041db52666e74696c88fafc62
LiatKotigaro <liat.kotigaro@verint.com>
2016-12-13 21:38:45 +0200

    Solution for: 14_functions_lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -6,4 +6,22 @@ Write 2 functions:
     mymul - returns the multiplication of its input arguments
     Ignore non-numeric arguments
 """
+import sys
 
+def mysum(*numbers):
+    res = 0.0
+    for number in numbers:
+        if type(number) == type(1):
+            res = res + number
+        if type(number) == type(1.0):
+            res = res + number
+    return res
+
+def mymul(*numbers):
+    res = 1.0
+    for number in numbers:
+        if type(number) == type(1):
+            res = res * number
+        if type(number) == type(1.0):
+            res = res * number
+    return res
