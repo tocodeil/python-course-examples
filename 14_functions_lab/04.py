3ac96250da1d64e8baf4aadfbf8a594e5f802e58
LiatKotigaro <liat.kotigaro@verint.com>
2016-12-13 23:23:47 +0200

    Solution for: 14_functions_lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -4,9 +4,15 @@ a list of words, and returns only the words
 longer than minlen
 """
 
-def longer_than():
+def longer_than(length, *words):
+    if type(length) != type(1):
+         raise Exception("First parameter should be a number")
+    res = []
+    for word in words:
+        if type(word) != type('str'):
+            raise Exception("All parameters except from the first parameter should be a strings")
+        if len(word) > length:
+            res.append(word)
+    return res
     pass
-
-
-# print longer_than(4, "foo", "bar", "fantastic", "python", "abc")
 
