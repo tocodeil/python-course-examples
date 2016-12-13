84f694c1709b8417a733855340b4e8c4b976569d
LiatKotigaro <liat.kotigaro@verint.com>
2016-12-13 22:50:25 +0200

    Solution for: 14_functions_lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -4,8 +4,13 @@ of the 10th digit from all arguments passed to it
 """
 
 
-def sum_tens():
+def sum_tens(*params):
+    res = 0
+    index = 1
+    for param in params:
+        if type(param) != type(1):
+            raise Exception("All parameters should be integers. Parameter " + str(index) + " is not an integer")
+        res += (param / 10) % 10    
+        index += 1
+    return res
     pass
-
-
-# print sum_tens(120, 140, 1123)
