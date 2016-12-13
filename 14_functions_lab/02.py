9df4108b66f7b9c86673ff47796a452699744b3a
shlomiiss <shlomiiss@gmail.com>
2016-12-13 12:07:37 +0200

    Solution for: 14_functions_lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,10 +1,21 @@
-"""
-Write a function called take_string_and_number that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+""" 
+Write a function called take_string_and_number that takes two arguments:  
+    A string And a number 
+ If wrong types were passed in, raise an exception 
+""" 
+def take_string_and_number(s,n): 
+    badstringflag=0
+    badnumberflag=0
+    if type(s)!=str:
+       badstringflag=1
+    if type(n)!=int:
+       badnumberflag=1   
+    
+    if (badstringflag+badnumberflag)>=1:
+      raise Exception("Bad inputs of string and/or numbers were entered")
+    else:
+      print "inputs are valid"
+      
 
-def take_string_and_number(s,n):
-    pass
 
+take_string_and_number('1',9) 
