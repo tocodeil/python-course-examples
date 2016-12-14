a1917b002af52408a5562ffdbc9a0057c28a908b
User1978 <User1978@users.noreply.github.com>
2016-12-14 16:11:35 +0200

    Solution for: 14_functions_lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -4,9 +4,16 @@ a list of words, and returns only the words
 longer than minlen
 """
 
-def longer_than():
-    pass
+def longer_than(minlen, *words):
+    tword = ''
+    for word in words:
+        if len(word) > minlen:
+            tword = tword + '\n' + word
+            #print word
+    return tword
 
 
-# print longer_than(4, "foo", "bar", "fantastic", "python", "abc")
+
+
+print longer_than(4, "foo", "bar", "fantastic", "python", "abc")
 
