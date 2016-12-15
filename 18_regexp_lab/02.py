728ee92a72dc37acb49570d9d8821dc1e18583c7
igalzzz <igal22@gmail.com>
2016-12-15 23:05:25 +0200

    Solution for: 18_regexp_lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -4,9 +4,10 @@ Write a function to_underscore(text) that returns the text with
 underscores between words
 """
 
-test1 = "LifeMustBeLivedAsPlay"
-tesxt2 = "Life_must_be_lived_as_play"
+#test1 = "life must be lived as play"
 
+import re
+ 
 def  toCamelCase(text):
 	spaced = re.sub('_', ' ', text)
 	UpperCase = re.sub(r'\s\b([a-z])', lambda m: m.group(1).upper(), spaced)
@@ -18,3 +19,7 @@ def to_underscore(text):
 	spaced = re.sub(r'([A-Z])', lambda m: " " + m.group(1).lower(), text)
 	UnderScored = re.sub(' ', '_', spaced)
 	return UnderScored
+
+	
+#print toCamelCase (test1)
+#print to_underscore (test1)
