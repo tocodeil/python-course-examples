2c3768318ce47a3bb8614cf95b8446d59cb4face
igalzzz <igal22@gmail.com>
2016-12-15 23:09:17 +0200

    Solution for: 18_regexp_lab/03.py

--- 18_regexp_lab/03.py
+++ 18_regexp_lab/03.py
@@ -1,88 +1,25 @@
 """
-Following code assumes a class named Widget that depends on
-other widgets
-Widget constructor takes a name, and function 
-add_dependency(depends)
-marks widget as depends on all widgets in "depends" list
+Write a python program that takes a CSV file
+reads it line by line and prints each line
+with first and second columns reversed.
 
-When building a widget, you need to first build all the widgets it
-depends on.
+Sample input:
+    Shana,Sargent,shanasargent@isoswitch.com
+    Witt,Hampton,witthampton@zaphire.com
+    Morgan,Grant,morgangrant@lotron.com
 
-Implement widget so the following code works:
-
-luke    = Widget("Luke")
-hansolo = Widget("Han Solo")
-leia    = Widget("Leia")
-yoda    = Widget("Yoda")
-padme   = Widget("Padme Amidala")
-anakin  = Widget("Anakin Skywalker")
-obi     = Widget("Obi-Wan")
-darth   = Widget("Darth Vader")
-_all    = Widget("All")
-
-
-luke.add_dependency(hansolo, leia, yoda)
-leia.add_dependency(padme, anakin)
-obi.add_dependency(yoda)
-darth.add_dependency(anakin)
-
-_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
-
-# print all widgets so each widget is printed before
-# the ones it depends on
-_all.build()
+Sample output:
+    Sargent,Shana,shanasargent@isoswitch.com
+    Hampton,Witt,witthampton@zaphire.com
+    Grant,Morgan,morgangrant@lotron.com
 """
 
-class Widget (object):
-	
-	def __init__(self, name):
-		self._name = name
-		self._others = []
 
-		
-	def add_dependency(self, *others):
-		self._others += others
-	
-	
-	def build_depnds_list(self):
-		dependents = [self._name]
-		for other in self._others:
-			dependents += other.build_depnds_list()
-		return dependents
-		
-	
-	def build(self):
-		dependents = self.build_depnds_list()[1:]
-		dependents = list(set(dependents))
-		
-		for dependent in dependents[:len(dependents)-1]:
-			print "%s," % dependent	
-		print "%s," % dependents[-1]
-		
-'''		
-luke    = Widget("Luke")
-hansolo = Widget("Han Solo")
-leia    = Widget("Leia")
-yoda    = Widget("Yoda")
-padme   = Widget("Padme Amidala")
-anakin  = Widget("Anakin Skywalker")
-obi     = Widget("Obi-Wan")
-darth   = Widget("Darth Vader")
-_all    = Widget("All")
+import csv
+import sys
 
-
-luke.add_dependency(hansolo, leia, yoda)
-leia.add_dependency(padme, anakin)
-obi.add_dependency(yoda)
-darth.add_dependency(anakin)
-
-_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
-
-# print all widgets so each widget is printed before
-# the ones it depends on
-_all.build()'''
-		
-		
-		
-		
-		
+fileName = sys.argv[1]
+with open(fileName + ".csv" , 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row[1] + ",", row[0] + ",", row[2]
