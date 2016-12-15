74069f2163c3599746199f94108e68bc59dbda45
igalzzz <igal22@gmail.com>
2016-12-15 22:30:39 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -10,3 +10,15 @@ Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
 
+import re
+import sys
+
+fileName = sys.argv[1]
+key =  sys.argv[2]
+
+
+with open (fileName, 'r') as f:
+	for line in f:
+		search = re.search(r'(.*\S)\s*\=\s*(\w+)',line)
+        if search is not None and search.group(1) == key:
+            print "The value of key: %s is: %s" % (key ,search.group(2))
