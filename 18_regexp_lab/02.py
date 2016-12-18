81db7dfb68a6baad14ee472a88bc0eea7b6e14ea
shlomiiss <shlomiiss@gmail.com>
2016-12-18 13:56:10 +0200

    Solution for: 18_regexp_lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -1,6 +1,17 @@
 """
 Write a function toCamelCase(text) that returns the text camel cased
 Write a function to_underscore(text) that returns the text with 
-underscores between words
-"""
+underscores between word
 
+ toCamelCase(welcome) => welcome
+toCamelCase(hello_world) => helloWorld
+toCamelCase(get_name) => getName
+toCamelCase(no_more_shall_we_part) => noMoreShallWePart
+"""
+import re
+def toCamelCase(text):
+    return re.sub(r'\_([a-z])', lambda m: m.group(1).upper(),text)
+    
+
+
+print toCamelCase('get_name')
