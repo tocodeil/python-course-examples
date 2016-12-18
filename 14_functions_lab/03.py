d79dc65d76fff6b0449c31ceaa8241798768f55c
shlomiiss <shlomiiss@gmail.com>
2016-12-18 13:55:21 +0200

    Solution for: 14_functions_lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -5,16 +5,16 @@ of the 10th digit from all arguments passed to it
 
 
 def sum_tens(*numbers):
- numberslist = list(numbers)
- i=''
- sum1=0
- tendigit=0
- for index in range(len(numberslist)):
-   i = str(numberslist[index])
-   if len(i)>=2:
-     tendigit = int(i[-2])
-     sum1 +=tendigit   
- return sum1
+    numberslist = list(numbers)
+    i=''
+    sum1=0
+    tendigit=0
+    for index in range(len(numberslist)):
+        i = str(numberslist[index])
+        if len(i)>=2:
+            tendigit = int(i[-2])
+            sum1 +=tendigit   
+    return sum1
        
   
 
