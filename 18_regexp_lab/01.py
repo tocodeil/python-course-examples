876ac8c04739a8916d1e79673623e35a4c0827c4
shlomiiss <shlomiiss@gmail.com>
2016-12-18 13:56:07 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -5,8 +5,19 @@ Each line in the file can either be:
     An empty line
     A comment line (Start with #)
     A property line (of the form key = value)
-
 Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
-
+import sys
+import re
+(filename, key) = sys.argv[1:]
+keys = {}
+data=[]
+with open(filename, "r") as fin1:
+    for line in fin1:
+        match = re.search('^[a-z]*\s*=\s*[a-z]*[0-9]*',line)
+        if match:
+            data = match.group(0).split('=')
+            data = [x.strip(' ') for x in data]  
+            keys[data[0]]=data[1]
+print keys.get(key)
