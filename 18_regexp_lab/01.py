5d8a60b6d8427da0bde9ac7ffa396a6ae4a04925
User1978 <User1978@users.noreply.github.com>
2016-12-19 10:53:44 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -10,3 +10,23 @@ Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
 
+import sys
+import re
+
+file_name = sys.argv[1]
+my_key = sys.argv[2:]
+grouped = {}
+
+with open(file_name, 'r') as property:
+    for line in property:
+        locate = re.search(r'(\w+)\s*=\s*(\w+)', line)
+
+        if locate:
+            keys = locate.group(1)
+            values = locate.group(2)
+            grouped[keys] = values
+
+    #print grouped
+
+for item in my_key:
+    print grouped[item]
