0de26522b6907229a9a2b5bb425295dc37a96192
anatshapira <anatshapira8@gmail.com>
2016-12-19 21:12:21 +0200

    Solution for: 05_syntax_lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -4,19 +4,14 @@ syntax_ex01.py
 """
 
 i = 0
-max_num = 0 
-new_num = 0
+max_num = float('-inf')
+
 while i < 10:
-
     print "Please enter a number:"
-    new_num = raw_input()
-    while not new_num.isdigit():
-        print "invalid input, try again... or cancel"
-        new_num = raw_input()
-
+    new_num = float(raw_input())
     if new_num > max_num:
         max_num = new_num
     i+=1
 
 
-print "your biggest number enterd so far is: " , max_num
+print "your biggest number enterd so far is: %f" % max_num
