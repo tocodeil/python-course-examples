04397ca99196afdfedd916bccb0066cfc3387ae9
shlomiiss <shlomiiss@gmail.com>
2016-12-19 12:18:47 +0200

    Solution for: 18_regexp_lab/03.py

--- 18_regexp_lab/03.py
+++ 18_regexp_lab/03.py
@@ -15,14 +15,8 @@ import csv
 import re
 data = []
 newformat = []
-with open ('input.csv', 'rb') as csvfile:
-    readline = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for line in readline:
-        match1 = re.search('\'[a-zA-Z]*',str(line))
-        match2 = re.search('\,[a-zA-Z]*\,',str(line))
-        match3= re.search('[a-zA-Z]*@[A-Za-z]*\.[A-Za-z]*',str(line))
-        newformat.append(match2.group(0))
-        newformat.append(match1.group(0))
-        newformat.append(match3.group(0))
-print newformat
-        
+with open ('input.csv', 'r') as fin:
+    for line in fin:
+        match = re.split(',',line)
+        print ("%s" % match[1],match[0],match[2])
+
