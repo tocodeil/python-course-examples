7a42ee9f582cb12623f2b8223e47c348f7d34dad
anatshapira <anatshapira8@gmail.com>
2016-12-20 21:51:35 +0200

    Solution for: 06_modules_lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -13,9 +13,12 @@ if len(sys.argv) != 3:
 
 for root, dirs, files in os.walk(folder_path):
     for name in files:
-        if os.stat(root + "\/" + name).st_size/1024/1024 > file_size:
-            print "file  %s size is:%s" % (name, os.stat(root + "\/" + name).st_size/1024/1024)
+        print os.stat(root + "\/" + name).st_size/1024/1024
+        if os.stat(root + "\/" + name).st_size/1024/1024 > float(file_size):
+            print "file  %s size is:%s MB" % (name, os.stat(root + "\/" + name).st_size/1024/1024)
             print "do you wish to delete file ? (Y/N)"
             to_delete=raw_input()
-            if to_delete.upper == "Y":
+            
+            
+            if to_delete.upper() == "Y":
                 os.remove(root + "\/" + name)
