4c19442303f1821e8a3f32ca8644246b12140302
User1978 <User1978@users.noreply.github.com>
2016-12-20 09:45:06 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -3,8 +3,8 @@ Write a program that reads data
 from property files.
 Each line in the file can either be:
     An empty line
-    A comment line (Start with #)
-    A property line (of the form key = value)
+    A comoment line (Start with #)
+    A property line (f the form key = value)
 
 Write a program that takes a property file name and key
 as command line arguments and prints the requested value
@@ -19,14 +19,15 @@ grouped = {}
 
 with open(file_name, 'r') as property:
     for line in property:
-        locate = re.search(r'(\w+)\s*=\s*(\w+)', line)
+        #locate = re.search(r'(\w+)\s*=\s*(\w+)', line)
+        locate = re.search(r'([a-z.]*)\s*=\s*(\w+)', line)
 
         if locate:
             keys = locate.group(1)
             values = locate.group(2)
             grouped[keys] = values
 
-    #print grouped
+    #print "grouped: ", grouped
 
 for item in my_key:
     print grouped[item]
