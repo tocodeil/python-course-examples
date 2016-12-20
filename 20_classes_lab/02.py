db99c2f6d7040761450d87dd8b8cc535437bbdb9
Chagit171 <Chagit171@gmail.com>
2016-12-20 19:57:36 +0200

    Solution for: 20_classes_lab/02.py

--- 20_classes_lab/02.py
+++ 20_classes_lab/02.py
@@ -11,3 +11,21 @@ following code:
 should print 10
 """
 
+
+class MyCounter(object):
+
+    count = 0
+
+    def __init__(self):
+        MyCounter.count+=1
+        
+
+
+
+
+
+for _ in range(10):
+     c1 = MyCounter()
+
+# should print 10
+print MyCounter.count
