83cbde1ca349216063b3df109e3afb98d8159fb5
anatshapira <anatshapira8@gmail.com>
2016-12-20 21:51:36 +0200

    Solution for: 06_modules_lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -6,10 +6,11 @@
 import sys
 
 
-print "How many Hello Python would you like to see today ? "
+#print "How many Hello Python would you like to see today ? "
 
-times = int(raw_input())    
-
+#times = int(raw_input())    
+times = int(sys.argv[1])
 while times > 0:
     print "Hello Python"
     times-=1
+    
