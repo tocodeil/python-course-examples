e69ec25cd9aa7f5adfce36512ee7689400d5fcf8
Chagit171 <Chagit171@gmail.com>
2016-12-20 17:52:29 +0200

    Solution for: 18_regexp_lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -10,3 +10,20 @@ Write a program that takes a property file name and key
 as command line arguments and prints the requested value
 """
 
+import sys
+import re
+
+
+def value_from_key(line, key_name):
+    if re.search("^" + key_name, line) is not None:
+        return True
+    else : return False
+            
+
+file = sys.argv[1]
+key = sys.argv[2]
+
+with open(file, "r") as fout:
+        for line in fout:
+            if value_from_key(line, key):
+                sys.stdout.write(re.split("=[ ]*", line)[1])
