c1b29e836f017e32894136e21740bc1577d6fa69
anatshapira <anatshapira8@gmail.com>
2016-12-21 11:48:15 +0200

    Solution for: 08_files_lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,26 +1,30 @@
 """
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name/
-The program should write the lines from the first two 
-files interwinded into the output file. So if we have
-
-a.txt with
-a1
-a2
-
-and b.txt with
-b1
-b2
-
-then running
-
-python 02.py a.txt b.txt c.txt
-will result in the creation of c.txt with the following content:
-
-a1
-b1
-a2
-b2
+ex02.py
+merge 2 files into a third file
 
 """
+
+import sys
+
+line1 = " "
+line2 = " "
+(_, file1, file2, file3) = sys.argv
+
+f1 = open(file1,'r') 
+f2 = open(file2,'r') 
+f3 = open(file3,'w') 
+
+line1 = f1.readline()
+line2 = f2.readline()
+
+while line1 != "" or line2 != "":
+    f3.write(line1)
+    f3.write(line2)
+    line1 = f1.readline()
+    line2 = f2.readline()
+
+
+f1.close()
+f2.close()
+f3.close()            
 
