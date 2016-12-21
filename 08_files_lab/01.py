d094927c4eb28037f7e857433fcd682ff99fc234
anatshapira <anatshapira8@gmail.com>
2016-12-21 11:48:13 +0200

    Solution for: 08_files_lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,34 +1,16 @@
 """
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one.
-
-So if file 'a.txt' has:
-
-a1
-a2
-
-and file 'b.txt' has
-
-b2
-b2
-b3
-
-then after running
-   python 01.py a.txt b.txt
-a.txt will have
-
-a1
-a2
-b1
-b2
-b3
-
-and b.txt will have
-
-b1
-b2
-b3
+ex0.py
+append file
 
 """
 
+import sys
+
+
+(_, file1, file2) = sys.argv
+
+with open(file2,'r') as f2:
+    with open(file1,'a') as f1:
+        for line in f2:
+            f1.write(line)
+            
