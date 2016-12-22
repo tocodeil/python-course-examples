355018e817132e3cec9e01fad6242a15f3319913
igalzzz <igal22@gmail.com>
2016-12-22 16:41:46 +0200

    Solution for: 25_exceptions_lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -4,4 +4,19 @@ and for each number prints its square root
 If value is negative or not a number show 
 a warning and keep reading values
 """
+import math
 
+while True:
+	print("Please enter your number: ")
+	inputstr = raw_input()
+	if inputstr == "":
+		break
+	try:
+		number = float(inputstr)
+	except Exception as e:
+		print "Warning: The value is not a number. Error: " , e
+		continue
+	try:
+		print "square root %f" % math.sqrt(number)
+	except Exception as e:
+		print "Warning: The value is a negative number. Error: " , e
