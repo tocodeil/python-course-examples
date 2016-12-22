2b5286d85fe12813ac7cfe7af59f51289ebf2ef6
LiatKotigaro <liat.kotigaro@verint.com>
2016-12-22 16:51:09 +0200

    Solution for: 27_tictactoe/main.py

--- 27_tictactoe/main.py
+++ 27_tictactoe/main.py
@@ -1,7 +1,7 @@
 """ Main project file
 """
 import lib.board as board
-from lib.player import Player
+import lib.player as player
 
 def PrintTable(game):
     print "%10s%10s%10s" % (game.val(0,0), game.val(0,1), game.val(0,2)) 
@@ -9,23 +9,31 @@ def PrintTable(game):
     print "%10s%10s%10s" % (game.val(2,0), game.val(2,1), game.val(2,2))
 
 
-print "please enter name for first player"
-name1 = raw_input()
-print "please enter tag (X or O) for first player"
-tag1 = raw_input()
-tag2 = ""
-if tag1 == 'X':
-    tag2 = 'O'
-else:
-    tag2 = 'X'
-print "please enter name for second player"
-name2 = raw_input()
-print "tag for second player will be ", tag2
-
+while True:
+    try:
+        print "please enter name for first player"
+        name1 = raw_input()
+        print "please enter tag (X or O) for first player"
+        tag1 = raw_input()
+        p1 = player.Player(name1, tag1)
+        tag2 = ""
+        if tag1 == 'X':
+            tag2 = 'O'
+        else:
+            tag2 = 'X'
+        print "please enter name for second player"
+        name2 = raw_input()
+        print "tag for second player will be ", tag2
+        p2 = player.Player(name2, tag2)
+    except player.InvalidPlayerTag  as e:
+        print e
+        continue
+    except BaseException  as e:
+        print e
+        continue
+    break 
 
 g = board.board.Game()
-p1 = Player(name1, tag1)
-p2 = Player(name2, tag2)
 
 g.start(p1, p2)
 
@@ -35,11 +43,19 @@ while(g.gameisover() == False):
         raw = int(raw_input())
         print "%s plase select move column between 0-2" % g.current_player._name
         column = int(raw_input())
-    except Exception  as e:
+        g.play(raw,column)
+    except board.InvalidPlayersType  as e:
         print e
         continue
-    g.play(raw,column)
+    except board.InvalidMove  as e:
+        print e
+        continue
+    except BaseException  as e:
+        print e
+        continue
+        
     PrintTable(g)
+    
 
 if g.tie() == True:
     print "The game ended with a tie between %s and %s" % (p1._name, p2._name)
