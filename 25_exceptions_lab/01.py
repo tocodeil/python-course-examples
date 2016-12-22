d92383a74cb8a3888d1a1f3e956280cd85c21036
Chagit171 <Chagit171@gmail.com>
2016-12-22 18:41:39 +0200

    Solution for: 25_exceptions_lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -5,3 +5,22 @@ If value is negative or not a number show
 a warning and keep reading values
 """
 
+import math
+import sys
+
+
+while True:
+    a = raw_input()
+    if a == "": break  
+    try:
+        print math.sqrt(int(a))
+    except Exception as e:
+        print e.message
+   
+
+      
+
+
+         
+     
+        
