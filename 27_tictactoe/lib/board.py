14a14676ceef4091fa888466a2be2611c20fb52e
User1978 <User1978@users.noreply.github.com>
2016-12-22 16:19:02 +0200

    Solution for: 27_tictactoe/board.py

--- 27_tictactoe/lib/board.py
+++ 27_tictactoe/lib/board.py
@@ -1,81 +1,65 @@
 from itertools import cycle
 
-class InvalidMove(Exception): pass
+
+class InvalidMove(Exception):
+    pass
+
 
 class Game(object):
-    boardmarks =  [[[''], [''], ['']], [[''], [''], ['']], [[''], [''], ['']]]
-    #winner = False
+    boardmarks = [[[''], [''], ['']], [[''], [''], ['']], [[''], [''], ['']]]
+    players = ''
+    current_player = ''
 
     def __init__(self):
-        #boardmarks = [[[''], [''], ['']], [[''], [''], ['']], [[''], [''], ['']]]
         self.clearBoard()
-        #self.winner() == False
         self.welcome_message()
 
-
-
-
-    def start(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-        self.players = cycle([p1, p2])
+    def start(self, pl1, pl2):
+        self.p1 = pl1
+        self.p2 = pl2
+        self.players = cycle((self.p1, self.p2))
         self.current_player = self.players.next()
-        #counter: count 8 game rounds. if no winner --> it's a tie
-        counter = 0
+        print "Player turn: ", self.current_player.name
 
-        self.clearBoard()
-
-        pass
+    def swap_turn(self):
+        self.current_player = self.players.next()
+        print "Player turn: ", self.current_player.name
 
     def play(self, row, column):
-        if row < 0 or row > 3 or column < 0 or column > 3:
+        if row < 0 or row > 2 or column < 0 or column > 2:
             raise InvalidMove("You have made an illegal move! Please choose a number between 1-3")
 
-        #availability testavail(row,column) -- in val below
-
-        #counter = counter + 1
-        pass
-
-    #@classmethod
     def winner(self):
-        self.winner = False
         #Check fow winner in each row --> 123 | 456 | 789 (+ mechanism for false winning if empty)
-        if (Game.boardmarks[0][0] == Game.boardmarks[0][1]) and (Game.boardmarks[0][2] == Game.boardmarks[0][1]) and (Game.boardmarks[0][0] != ''):
-            win = True
-            print "Win 1"
-        if (Game.boardmarks[1][0] == Game.boardmarks[1][1]) and (Game.boardmarks[1][2] == Game.boardmarks[1][1]) and (Game.boardmarks[1][0] != ''):
-            win = True
-            print "Win 2"
-        if (Game.boardmarks[2][0] == Game.boardmarks[2][1]) and (Game.boardmarks[2][2] == Game.boardmarks[2][1]) and (Game.boardmarks[2][0] != ''):
-            win = True
-            print "Win 3"
+        if (self.boardmarks[0][0] == self.boardmarks[0][1]) and (self.boardmarks[0][2] == self.boardmarks[0][1]) and (self.boardmarks[0][0] != ['']):
+            #print self.boardmarks[0][0], self.boardmarks[0][1], self.boardmarks[0][2]
+            print "You Won!"
+        elif (self.boardmarks[1][0] == self.boardmarks[1][1]) and (self.boardmarks[1][2] == self.boardmarks[1][1]) and (self.boardmarks[1][0] != ['']):
+            print "You Won!"
+        elif (self.boardmarks[2][0] == self.boardmarks[2][1]) and (self.boardmarks[2][2] == self.boardmarks[2][1]) and (self.boardmarks[2][0] != ['']):
+            print "You Won!"
         # Check fow winner in each column --> 147 | 258 | 369 (+ mechanism for false winning if empty)
-        if (Game.boardmarks[0][0] == Game.boardmarks[1][0]) and (Game.boardmarks[2][0] == Game.boardmarks[1][0]) and (Game.boardmarks[0][0] != ''):
-            win = True
-            print "Win 4"
-        if (Game.boardmarks[0][1] == Game.boardmarks[1][1]) and (Game.boardmarks[2][1] == Game.boardmarks[1][1]) and (Game.boardmarks[0][1] != ''):
-            win = True
-            print "Win 5"
-        if (Game.boardmarks[0][2] == Game.boardmarks[1][2]) and (Game.boardmarks[2][2] == Game.boardmarks[1][2]) and (Game.boardmarks[0][2] != ''):
-            win = True
-            print "Win 6"
+        elif (self.boardmarks[0][0] == self.boardmarks[1][0]) and (self.boardmarks[2][0] == self.boardmarks[1][0]) and (self.boardmarks[0][0] != ['']):
+            print "You Won!"
+        elif (self.boardmarks[0][1] == self.boardmarks[1][1]) and (self.boardmarks[2][1] == self.boardmarks[1][1]) and (self.boardmarks[0][1] != ['']):
+            print "You Won!"
+        elif (self.boardmarks[0][2] == self.boardmarks[1][2]) and (self.boardmarks[2][2] == self.boardmarks[1][2]) and (self.boardmarks[0][2] != ['']):
+            print "You Won!"
         # Check fow winner in diagonal --> 159 | 357 (+ mechanism for false winning if empty)
-        if (Game.boardmarks[0][0] == Game.boardmarks[1][1]) and (Game.boardmarks[2][2] == Game.boardmarks[1][1]) and (Game.boardmarks[0][0] != ''):
-            win = True
-            print "Win 7"
-        if (Game.boardmarks[0][2] == Game.boardmarks[1][1]) and (Game.boardmarks[2][0] == Game.boardmarks[1][1]) and (Game.boardmarks[0][2] != ''):
-            win = True
-            print "Win 8"
-        return win
-
+        elif (self.boardmarks[0][0] == self.boardmarks[1][1]) and (self.boardmarks[2][2] == self.boardmarks[1][1]) and (self.boardmarks[0][0] != ['']):
+            print "You Won!"
+        elif (self.boardmarks[0][2] == self.boardmarks[1][1]) and (self.boardmarks[2][0] == self.boardmarks[1][1]) and (self.boardmarks[0][2] != ['']):
+            print "You Won!"
+        else:
+            return None
 
     def val(self, row, column):
-        occupy = self.data[row][column]
-        if not occupy:
-            return False
+        if self.boardmarks[row][column] == ['']:
+            #print "true"
+            return True
         else:
-            return occupy.val
-
+            #print "false"
+            return False
 
     def clearBoard(self):
         self.boardmarks = [[[''], [''], ['']], [[''], [''], ['']], [[''], [''], ['']]]
