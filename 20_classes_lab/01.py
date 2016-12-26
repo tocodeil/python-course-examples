31b74883d050bbc8debf5606ad3631e1aaedc94f
Lionellionelel <lionelel@gmail.com>
2016-12-26 14:02:28 +0200

    Solution for: 20_classes_lab/01.py

--- 20_classes_lab/01.py
+++ 20_classes_lab/01.py
@@ -1,6 +1,25 @@
-"""
-Write a class called "Summer" that provides the methods:
-    add(n) - adds a number n to the sum
-    total() - returns total sum of numbers added so far
-"""
 
+class Summer(object):
+    def __init__(self):
+        self.number = 0
+
+    def add(self, *args):
+        for n in args:
+            self.number += n
+
+    def total(self):
+        return self.number
+
+
+s = Summer()
+t = Summer()
+
+s.add(10, 20)
+t.add(50)
+s.add(30)
+
+# should print 60
+print s.total()
+
+# should print 50
+print t.total()
