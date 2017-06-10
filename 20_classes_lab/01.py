862fa77c51a16185eac2cf0d2fc95c1321c1a514
ynonp <ynonperek@gmail.com>
2017-06-11 00:07:43 +0300

    FIXED description

--- 20_classes_lab/01.py
+++ 20_classes_lab/01.py
@@ -1,6 +1,7 @@
 """
 Write a class called "Summer" that provides the methods:
     add(n) - adds a number n to the sum
+    add(x, y, z, ...) - adds all numbers passed as input arguments to the sum
     total() - returns total sum of numbers added so far
 """
 
