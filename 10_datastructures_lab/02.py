e94a284474729283eccca0a6651d6f2acd2b4ae3
ynonp <ynon@ynonperek.com>
2017-06-10 23:55:33 +0300

    Create 02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,9 +1,8 @@
 """
-Define a list of 20 grades
-and print only the grades that
-are above average
-"""
+Accept a list of 20 grades on the command line and print the grades that
+are above average. e.g.
 
-grades = [99, 90, 15, 28, 38, 44, 50, 81, 79, 60, 99, 90, 15, 28, 38, 44, 50,
-        81, 79, 60 ]
+python 02.py 99 90 15 28 38 44 50 81 79 60 99 90 15 28 38 44 50 81 79 60
+will print 99 90 81 79 60 99 90 81 79 60
+"""
 
