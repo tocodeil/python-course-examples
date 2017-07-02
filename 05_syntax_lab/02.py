4d20f0ce88e0741f6a5bd3fc72c2a2b1b9f0d72c
mikeler216 <mikelerner216@gmail.com>
2017-07-02 16:50:44 +0300

    Solution for: 05_syntax_lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 """Takes value of age in years returns value in months"""
-print 'age in months: %d' % (int(raw_input('input age in years: ')) * 12)
+print '%d' % (int(raw_input()) * 12)
