29d8f132fef30cd90d8090298ea7526c35f251f0
ynonp <ynon@ynonperek.com>
2017-07-08 21:38:58 +0300

    Update 01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -7,13 +7,3 @@ is 74, program should print:
 
     Max number = 74
 """
-
-maxnum = float('-inf')
-
-for _ in range(10):
-    num = float(raw_input())
-    if num > maxnum:
-        maxnum = num
-
-print "Max number = %f" % maxnum
-
