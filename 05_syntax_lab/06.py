9bc836aa714f2a42ac8a1e0ef19742b520029b14
maornesimi <maornesimi1@gmail.com>
2017-07-10 21:09:14 +0300

    Solution for: 05_syntax_lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,15 @@
-"""
-Write a program that generates 2 random numbers
-and calculates their least common multiple,
-that is the smallest number that is divisible
-by both.
-For example if the numbers were 4 and 6,
-program should print 12.
-"""
-
+""" create 2 random numbers between 0-10, and return the small devider of them"""
+from random import randint
+x = randint(1,10)
+y = randint(1,10)
+if x > y :
+    for i in range(x,100):
+        if i % x == 0 and i % y == 0:
+            break
+else:
+    for i in range(y,100):
+        if i % x == 0 and i % y == 0:
+            break
+print "the numbers %s , %s" %(x,y)
+print "the small devier:", i 
+     
