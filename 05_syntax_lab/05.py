b63b29cb74f8927b2b836f85d550b363e3384bc8
maornesimi <maornesimi1@gmail.com>
2017-07-10 21:09:10 +0300

    Solution for: 05_syntax_lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,11 @@
-"""
-Write a program that generates random integers in a loop
-until it finds a number that is divisible by: 7, 13, and 15.
-"""
-
+""" find random number that devide in 7,13, and 15"""
+from random import randint
+i=0
+helpy = 0
+while helpy == 0:
+    x = randint(1, 1000000)
+    i +=1
+    if x % 7 == 0 and x % 13 == 0 and x % 15 == 0 :
+        helpy = 1
+print "our number:", x
+print "numbers of tries:", i
