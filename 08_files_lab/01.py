0e32b10dbee21bc314895cdba00971eac5cf6dfa
mikeler216 <mikelerner216@gmail.com>
2017-07-12 17:38:03 +0300

    Solution for: 08_files_lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,34 +1,28 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one.
+#!/usr/bin/python
+import sys, os
 
-So if file 'a.txt' has:
+if len(sys.argv) == 1:
+	sys.exit('usage %s <text file1> <text file2(optinal)>' % sys.argv[0])
 
-a1
-a2
-
-and file 'b.txt' has
-
-b1
-b2
-b3
-
-then after running
-   python 01.py a.txt b.txt
-a.txt will have
-
-a1
-a2
-b1
-b2
-b3
-
-and b.txt will have
-
-b1
-b2
-b3
-
-"""
+if len(sys.argv) == 2:
+	with open(sys.argv[1]) as f1:
+		Text1 = f1
+		with open('newFile.txt', 'w') as f2:
+			for line in Text1:
+				f2.write(line)
+	sys.exit()			
+elif len(sys.argv) == 3:
+	with open(sys.argv[1]) as f1:
+		Text1 = f1
+		with open(sys.argv[2], 'a') as f2:
+			for line in Text1:
+				f2.write(line)
+elif len(sys.argv) == 4:
+	with open(sys.argv[1]) as f1:
+		with open(sys.argv[2]) as f2:
+			with open(sys.argv[3], 'a') as f3:
+				for line in f1:
+					f3.write(line)
+				for line in f2:
+					f3.write(line)
 
