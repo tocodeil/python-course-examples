405da47878e87bde2eb92428699044ffe520994a
maornesimi <maornesimi1@gmail.com>
2017-07-23 05:18:46 +0300

    Solution for: 06_modules_lab/02_bonus.py

--- 06_modules_lab/02_bonus.py
+++ 06_modules_lab/02_bonus.py
@@ -1,15 +1,12 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-Bonus: Print error messages for invalid inputs.
-
-To print error messages we'll have to use a concept not yet learned in the
-course, and which will only be presented later: Exceptions.
-We'll tap into python's error handling and change its default
-error message to something more meaningful.
-
-Read ahead the exception chapter here:
-https://docs.python.org/2.7/tutorial/errors.html
-
-And then continue to writing the code
-"""
-
+""" get ywo nuber and print their sum + check"""
+import os,sys
+if len(sys.argv) != 3 :
+    print "we need exactlly 2 numbers"
+    quit()
+try:
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+except ValueError:
+    print "please insert only numbers"
+    quit()
+print "the result is:", x + y
