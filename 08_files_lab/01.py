0fce2ec0eff7654e4e28dc5e92f09fc12644f1f5
maornesimi <maornesimi1@gmail.com>
2017-07-23 13:17:06 +0300

    Solution for: 08_files_lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,34 +1,27 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one.
-
-So if file 'a.txt' has:
-
-a1
-a2
-
-and file 'b.txt' has
-
-b1
-b2
-b3
-
-then after running
-   python 01.py a.txt b.txt
-a.txt will have
-
-a1
-a2
-b1
-b2
-b3
-
-and b.txt will have
-
-b1
-b2
-b3
-
-"""
-
+"""appent file to another"""
+import os,sys
+with open ('c:\python/week_2_2/file1.txt','w') as fout:
+    for i in range(6,11):
+        fout.write("I number ")
+        fout.write(str(i))
+        fout.write('\n')
+with open ('c:\python/week_2_2/file2.txt','w') as fout:
+    for i in range(1,6):
+        fout.write("I number ")
+        fout.write(str(i))
+        fout.write('\n')
+f1 = sys.argv[1]
+f2 = sys.argv[2]
+if not os.path.exists('c:\python/week_2_2/{0}'.format(f1)):
+    print "there is no file with %s name" %f1
+    quit()
+if not os.path.exists('c:\python/week_2_2/{0}'.format(f2)):
+   with open ('c:\python/week_2_2/{0}'.format(f2),'w') as fout:
+    for i in range(1,6):
+        fout.write("I number ")
+        fout.write(str(i))
+        fout.write('\n') 
+with open ('c:\python/week_2_2/{0}'.format(f2),'a' ) as fapp:
+    with open ('c:\python/week_2_2/{0}'.format(f1),'r'  ) as fin:
+        for line in fin.read():
+            fapp.write(line)
