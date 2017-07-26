099804bf5817c353cd0ccfda72fa6e1f4fa295b9
mikeler216 <mikelerner216@gmail.com>
2017-07-26 11:36:48 +0300

    Solution for: 18_regexp_lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -1,6 +1,10 @@
-"""
-Write a function toCamelCase(text) that returns the text camel cased
-Write a function to_underscore(text) that returns the text with 
-underscores between words
-"""
+#!/usr/bin/python
+import re 
+import sys, os
+text = 'no_more_shall_we_part'
+def under_scope_to_camelCase(text):
+	x = re.sub(r'\_[a-z]' ,lambda m: m.group(0).upper()  , text)
+	x = re.sub(r'_' , '' , x)
+	print x
 
+under_scope_to_camelCase(text)
