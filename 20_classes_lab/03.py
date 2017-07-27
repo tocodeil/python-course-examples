ddbc4aed64a10cb0a338c375bac4b52b1de67489
mikeler216 <mikelerner216@gmail.com>
2017-07-27 14:11:38 +0300

    Solution for: 20_classes_lab/03.py

--- 20_classes_lab/03.py
+++ 20_classes_lab/03.py
@@ -1,15 +1,12 @@
-"""
-Following code assumes a class named Widget that depends on
-other widgets
-Widget constructor takes a name, and function 
-add_dependency(depends)
-marks widget as depends on all widgets in "depends" list
-
-When building a widget, you need to first build all the widgets it
-depends on.
-
-Implement widget so the following code works:
-
+#!/usr/bin/python
+class Widget(object):
+	def __init__(self, name):
+		self.name = name
+	def add_dependency(self, *args):
+		self.dependency = [arg.name for arg in args]
+	def build(self):
+		print self.dependency
+		
 luke    = Widget("Luke")
 hansolo = Widget("Han Solo")
 leia    = Widget("Leia")
@@ -27,9 +24,6 @@ obi.add_dependency(yoda)
 darth.add_dependency(anakin)
 
 _all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
-
-# print all widgets so each widget is printed before
-# the ones it depends on
 _all.build()
-"""
-
+# code should print: Han Solo, Padme Amidala, Anakin Skywalker, Leia, Yoda, Luke, Obi-Wan, Darth Vader
+# (can print with newlines in between modules)
