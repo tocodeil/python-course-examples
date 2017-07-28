f265a73e4aa81f993544dce5d0e6d4afe8c86307
maornesimi <maornesimi1@gmail.com>
2017-07-28 16:48:53 +0300

    Solution for: 10_datastructures_lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,19 +1,8 @@
-"""
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
-
-When valid credentials are entered print:
-    Welcome Master
-
-And when invalid credentials are entered print:
-    INTRUDER ALERT
-
-and exit the program with a non-zero exit code
-"""
-
+"""get name and password and check if it correct"""
+import os,sys
+nep = {'apple' : "red" , 'lettuce' : "green" , 'lemon' : "yellow" , 'orange' : "orange"}
+for key in nep:
+    if sys.argv[1] == key and nep[key] == sys.argv[2] :
+        print "Welcome Master"
+        quit()
+print "INTRUDER ALERT"
