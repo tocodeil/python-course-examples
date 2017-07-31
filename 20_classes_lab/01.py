6d987602913db7417a042e644efe5f41f05cd16d
mikeler216 <mikelerner216@gmail.com>
2017-07-31 09:22:18 +0300

    Solution for: 20_classes_lab/01.py

--- 20_classes_lab/01.py
+++ 20_classes_lab/01.py
@@ -1,21 +1,2 @@
 #!/usr/bin/python
-class Summer(object):
-	def __init__(self):
-		self.x = 0
-	def add(self,*args):
-		self.x +=  sum([int(arg) for arg in args])
-	def total(self):
-		return self.x	
-
-s = Summer()
-t = Summer()
-
-s.add(10, 20)
-t.add(50)
-s.add(30)
-
-# should print 60
-print s.total()
-
-# should print 50
-print t.total()
+print str([chr(small) for small in range(97,123)]).strip('[]').replace("'" , "")
