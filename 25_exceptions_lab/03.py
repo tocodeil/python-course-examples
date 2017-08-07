f4c04bbba65735f58e343a0087c35699a98d7393
mikeler216 <mikelerner216@gmail.com>
2017-08-07 10:16:36 +0300

    Solution for: 25_exceptions_lab/03.py

--- 25_exceptions_lab/03.py
+++ 25_exceptions_lab/03.py
@@ -1,4 +1,28 @@
-"""
-Write code to make the following unit test pass
-"""
+#!/usr/bin/python
 
+import unittest
+
+class InvalidImageExt(Exception): pass
+
+class TestImageFile(unittest.TestCase):
+    def test_good_ext(self):
+        try:
+            img = ImageFile("file.png")
+        except InvalidImageExt:
+            self.fail("png should be a valid file extension")
+
+    def test_bad_ext(self):
+        with self.assertRaises(InvalidImageExt):
+            img = ImageFile("file.mp3")
+
+class ImageFile(object):
+	def __init__(self, name):
+		self.name = name
+		
+		if not self.name.endswith('.png'):
+			raise InvalidImageExt
+		else:
+			open(self.name, 'w+')
+				
+		
+unittest.main()
