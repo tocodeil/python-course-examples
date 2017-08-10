8e1f392041582594e4fc0196fd69d4cbf38c8533
Danil315 <daniluz1@gmail.com>
2017-08-10 14:59:58 +0300

    Solution for: 08_files_lab/03.py

--- 08_files_lab/03.py
+++ 08_files_lab/03.py
@@ -1,3 +1,13 @@
-# Given a CSV file with numbers in the second column - 
-# print the sum of the numbers.
-#
+#!/usr/bin/python
+import sys
+import csv
+
+fileToOpen = sys.argv[1]
+
+with open(fileToOpen, "r") as csvfile:
+    total = 0
+    csvFile = csv.reader(csvfile)
+    for row in csvFile:
+        i = row[1]
+        total += int(i)
+print total
