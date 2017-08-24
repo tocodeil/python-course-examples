6ea36035551cdec188c84bb54dc632de8fa1d045
Danil315 <daniluz1@gmail.com>
2017-08-24 19:09:59 +0300

    Solution for: 12_listcomprehension_lab/03.py

--- 12_listcomprehension_lab/03.py
+++ 12_listcomprehension_lab/03.py
@@ -1,5 +1,9 @@
-"""
-Find and print all possible sequences
-of 3 lowercase characters: aaa, aab, aac, ..., zzz
-"""
+#!/usr/bin/python
 
+from itertools import combinations_with_replacement
+
+a_to_z =[(chr(x)) for x in range(97, 123)]
+
+comb_list = [y for y in combinations_with_replacement(a_to_z, 3)]
+
+print comb_list
